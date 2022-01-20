fa3c2336522b67dbd07638511a507c900466c565
shangrex <rexhuang881019@gmail.com>
2022-01-20 23:53:12 +0800

    Time: 20 ms (7.59%), Space: 15 MB (8.66%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -15,18 +15,29 @@ public:
         else if(!list2)return list1;
         ListNode* n = new ListNode();
         ListNode*iter = n;
+        int flag = 0;
         while(list1 != NULL && list2 != NULL){
+            ListNode *tmp = new ListNode();
             if(list1->val < list2->val){
-                iter->next = list1;
+                tmp->val = list1->val;
                 list1 = list1->next;
+                flag =1;
             }
             else {
-                iter->next = list2;
+                tmp->val = list2->val;
                 list2 = list2->next;
+                flag = 2;
             }
+            iter->next = tmp;
             iter = iter->next;
         }
-        iter->next = list1 ? list1 : list2;
+        if(flag == 1){
+            // check list2
+            iter->next = list2;
+        }
+        else {
+            iter->next = list1;
+        }
         return n->next;
     }
 };
