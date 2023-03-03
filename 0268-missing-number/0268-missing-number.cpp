b8152777f870e120365414ba54ec34f614d622b8
shangrex <rexhuang881019@gmail.com>
2023-03-03 23:33:39 +0800

    Time: 18 ms (81.70%), Space: 18 MB (25.27%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,11 +1,24 @@
 class Solution {
-public:
-    int missingNumber(vector<int>& nums) {
+    // method 1 summarization
+    /*
+        int missingNumber(vector<int>& nums) {
         int n = nums.size(), sum_num, tmp=0;
         sum_num = (n*n+n)/2;
         for(int i = 0; i < nums.size(); i++){
             tmp += nums[i];
         }
         return sum_num - tmp;
+    }
+    */
+    // method 2 xor
+public:
+    int missingNumber(vector<int>& nums) {
+        int tmp = 0;
+        for(int i = 0; i < nums.size(); i++){
+            tmp ^= i;
+            tmp ^= nums[i];
+        }
+        tmp ^= nums.size();
+        return tmp;
     }
 };
