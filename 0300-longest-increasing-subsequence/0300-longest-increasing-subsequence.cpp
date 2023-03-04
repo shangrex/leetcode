d9f46f890f5d3f093b72ecb7cd6b26a9ea51c884
shangrex <rexhuang881019@gmail.com>
2023-03-04 15:45:56 +0800

    Time: 9 ms (91.00%), Space: 10.5 MB (57.10%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,5 +1,6 @@
 class Solution {
-public:
+// method 1 dp 
+/*
     int lengthOfLIS(vector<int>& nums) {
         vector<int>dp(nums.size()+1, 1);
         int rst = 1;
@@ -11,5 +12,27 @@ public:
             rst = max(dp[i], rst);
         }
         return rst;
+    }*/
+// greedy with binary search
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        if(nums.size()==0)return 0;
+        vector<int>dp;
+        dp.push_back(nums[0]);
+        for(int i = 1; i < nums.size(); i++){
+            if(dp.back() < nums[i])dp.push_back(nums[i]);
+            else if(dp.back()==nums[i])continue;
+            else {
+                // binary search and insert
+                int l = 0, r = dp.size()-1;
+                while(l<r){
+                    int mid = (l+r)/2;
+                    if(dp[mid]<nums[i])l=mid+1;
+                    else r=mid;
+                }
+                dp[l] = nums[i];
+            }
+        }
+        return dp.size();
     }
 };
