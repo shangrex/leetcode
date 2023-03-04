2bdca53c1c4172489b2b6ef4f341982d42ee1ddf
shangrex <rexhuang881019@gmail.com>
2023-03-04 23:28:36 +0800

    Time: 160 ms (5.03%), Space: 24.5 MB (5.01%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -8,8 +8,7 @@ public:
         for(int i = 1; i <= text1.length(); i++){
             for(int j = 1; j <= text2.length(); j++){
                 dp[i][j] = max({dp[i-1][j], dp[i][j-1]});
-                int tmp = text1[i-1]==text2[j-1]?dp[i-1][j-1]+1:dp[i-1][j-1];
-                dp[i][j] = max({dp[i][j], tmp});
+                dp[i][j] = text1[i-1]==text2[j-1]?dp[i-1][j-1]+1:dp[i][j];
             }
         }
         return dp[text1.length()][text2.length()];
