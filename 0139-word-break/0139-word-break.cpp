4c218a44e0472ef51ec8b55c5e9121faf39d66c6
shangrex <rexhuang881019@gmail.com>
2023-03-11 17:17:17 +0800

    Time: 46 ms (10.09%), Space: 7.5 MB (96.71%) - LeetHub

---
+++ 0139-word-break/0139-word-break.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        vector<bool>dp(s.size()+1, false);
+        dp[0] = true;
+        for(int i = 1; i <= s.length(); i++){
+            for(auto j : wordDict){
+                if(i >= j.length()){
+                    if(dp[i-j.length()]){
+                        string tmp = s.substr(i-j.length(), j.length());
+                        if(find(wordDict.begin(), wordDict.end(), tmp) != wordDict.end())dp[i] = true;
+                    }   
+                }
+            }
+        }
+        return dp[s.length()];
+    }
+};
