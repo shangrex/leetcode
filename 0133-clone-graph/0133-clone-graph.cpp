c7ed5ef39f2ff0d1c154c46e4b4864364754018c
shangrex <rexhuang881019@gmail.com>
2023-04-03 18:25:38 +0800

    Time: 3 ms (95.14%), Space: 8.7 MB (75.26%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.cpp
+++ 0133-clone-graph/0133-clone-graph.cpp
@@ -24,38 +24,40 @@ public:
     // DFS to triverse
     Node* cloneGraph(Node* node) {
         if(node == NULL)return NULL;
+        
         // record the visited node
         vector<int>visit(101, 0);
+        
         // transform original node to new copied node
         unordered_map<Node*, Node*>mp;
+        
         // initialize dfs, and use dfs to triverse origin map
         stack<Node*> st;
         st.push(node);
+        
         // construct initialize copied node
         Node *clone = new Node(node->val);
         mp[node] = clone; 
+        
         while(!st.empty()){
             Node* cur = st.top();
             st.pop();
-            // triverse node
+            // not visit
             if(visit[cur->val] == 0) {
                 visit[cur->val] = 1;
-                // cout << cur->val;
-                // not visit 
                 for(int i = 0; i < cur->neighbors.size(); i++){
+                    // add not visited node nearby
                     if(visit[cur->neighbors[i]->val] == 0){
-                        // cout << cur->neighbors[i]->val;
                         st.push(cur->neighbors[i]);
                     }
-                    // cout << cur->neighbors[i]->val;
+                    
+                    // construct copied node and connect
                     if(mp.find(cur->neighbors[i]) == mp.end()){
-                        // construct copied node and connect
                         Node* clone = new Node(cur->neighbors[i]->val);
                         mp[cur->neighbors[i]] = clone;
                     }
                     mp[cur]->neighbors.push_back(mp[cur->neighbors[i]]);
                 }
-                // cout << endl;
             }
         }
         return mp[node];
