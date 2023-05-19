6f0987f20c288e02fa07d18e2a62fb7a47f27671
shangrex <rexhuang881019@gmail.com>
2023-05-19 21:14:53 +0800

    Time: 4 ms (33.61%), Space: 7 MB (16.24%) - LeetHub

---
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        int dirc[4][3] = {{0, 1, m-1}, {1, 0, n-1}, {0, -1, m-1}, {-1, 0, n-2}};
+        vector<int>rst;
+        
+        int way = 0;
+        int i = 0, j = -1;
+        int cnt = 0;
+        while(cnt < m*n){
+            if(way == 0){
+                j += 1;
+                cnt ++;
+                rst.push_back(matrix[i][j]);
+            }
+            int step = dirc[way][2];
+            while(step > 0){
+                cnt++;
+                cout << i << " " << j << " " << step << " " << way << endl;
+                step -= 1;
+                i = i+dirc[way][0], j = j+dirc[way][1];
+                rst.push_back(matrix[i][j]);
+            }
+            dirc[way][2] -= 2;
+            way = (way+1)%4;
+           // cout << i << j << endl;
+    
+        }
+        return rst;
+    }
+};
