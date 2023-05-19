38a6fb5815f39d7d4c18a8143acf60d943d2982d
shangrex <rexhuang881019@gmail.com>
2023-05-19 18:43:26 +0800

    Time: 20 ms (26.67%), Space: 13 MB (99.53%) - LeetHub

---
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        bool first_element_row_zero = false;
+        bool first_element_col_zero = false;
+        // check row col first element if zero
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(matrix[i][j] == 0){
+                    if(i == 0)first_element_row_zero = true;
+                    if(j == 0)first_element_col_zero = true;
+                    matrix[i][0] = matrix[0][j] = 0;
+                }
+            }
+        }
+        
+        for(int i = 1; i < n; i++){
+            if(matrix[i][0] == 0){
+                for(int j = 0; j < m; j++){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        for(int j = 1; j < m; j++){
+            if(matrix[0][j] == 0){
+                for(int i = 0; i < n; i++){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        if(first_element_col_zero){
+            for(int i = 0; i < n; i++){
+                matrix[i][0] = 0;
+            }
+        }
+        if(first_element_row_zero){
+            for(int j = 0; j < m; j++){
+                matrix[0][j] = 0;
+            }
+        }
+    }
+};
