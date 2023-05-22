a585936dcd9c725c7092a126ac75ab258a8a6483
shangrex <rexhuang881019@gmail.com>
2023-05-22 20:31:23 +0800

    Time: 0 ms (100.00%), Space: 6.3 MB (52.73%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -2,26 +2,25 @@ class Solution {
 public:
     bool isValid(string s) {
         stack<char>check;
-        check.push('*');
         for(int i = 0; i < s.length(); i++){
             if(s[i] == '(' || s[i] == '{' || s[i] == '['){
                 check.push(s[i]);
                 continue;
             }
             if(s[i] == ')'){
-                if(check.top() == '(')check.pop(); 
+                if(!check.empty() && check.top() == '(')check.pop(); 
                 else return false;
             }
             else if(s[i] == '}'){
-                if(check.top() == '{')check.pop(); 
+                if(!check.empty() && check.top() == '{')check.pop(); 
                 else return false;
             }
             else if(s[i] == ']'){
-                if(check.top() == '[')check.pop(); 
+                if(!check.empty() && check.top() == '[')check.pop(); 
                 else return false;
             }
         }
-        if(check.top() != '*')return false;
+        if(!check.empty())return false;
         return true;
     }
 };
