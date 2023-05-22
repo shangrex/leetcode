1fa45f6b7bb2e5827e1c0e501f96c625f0388f00
shangrex <rexhuang881019@gmail.com>
2023-05-22 20:13:21 +0800

    Time: 4 ms (39.20%), Space: 6.2 MB (81.53%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        if(s.length()%2==1)return false;
+        stack<char>check;
+        check.push('*');
+        for(int i = 0; i < s.length(); i++){
+            if(s[i] == '(' || s[i] == '{' || s[i] == '['){
+                check.push(s[i]);
+                continue;
+            }
+            if(s[i] == ')'){
+                if(check.top() == '(')check.pop(); 
+                else return false;
+            }
+            if(s[i] == '}'){
+                if(check.top() == '{')check.pop(); 
+                else return false;
+            }
+            if(s[i] == ']'){
+                if(check.top() == '[')check.pop(); 
+                else return false;
+            }
+        }
+        if(check.top() != '*')return false;
+        return true;
+    }
+};
