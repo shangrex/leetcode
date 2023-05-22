77ee8dd02ecdf54bf677b6f50dc3fdff2a721499
shangrex <rexhuang881019@gmail.com>
2023-05-22 20:14:37 +0800

    Time: 0 ms (100.00%), Space: 6.4 MB (52.73%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     bool isValid(string s) {
-        if(s.length()%2==1)return false;
         stack<char>check;
         check.push('*');
         for(int i = 0; i < s.length(); i++){
