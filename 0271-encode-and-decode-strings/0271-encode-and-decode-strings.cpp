e3bec174a2a5ead32faded1fab20dc95a0eff6e3
shangrex <rexhuang881019@gmail.com>
2023-05-23 18:22:06 +0800

    Time: 45 ms (65.19%), Space: 23.3 MB (67.49%) - LeetHub

--- 0271-encode-and-decode-strings/0271-encode-and-decode-strings.cpp
+++ 0271-encode-and-decode-strings/0271-encode-and-decode-strings.cpp
@@ -4,18 +4,20 @@ public:
     // Encodes a list of strings to a single string.
     string encode(vector<string>& strs) {
         string rst;
+        char non_ascii = char(257);
         for(int i = 0; i < strs.size(); i++){
-            rst += strs[i]+ '\n';
+            rst += strs[i]+ non_ascii;
         }
         return rst;
     }
 
     // Decodes a single string to a list of strings.
     vector<string> decode(string s) {
+        char non_ascii = char(257);
         vector<string>rst;
         int tmp_head = 0;
         for(int i = 0; i < s.length(); i++){
-            if(s[i] == '\n'){
+            if(s[i] == non_ascii){
                 rst.push_back(s.substr(tmp_head, i-tmp_head));
                 tmp_head = i+1;
             }
