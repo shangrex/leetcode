11fb694f0f6dfd12eab49f1631fd5af270c3c998
shangrex <rexhuang881019@gmail.com>
2023-05-25 01:54:25 +0800

    Time: 22 ms (82.66%), Space: 29 MB (18.09%) - LeetHub

---
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool check(TreeNode*root, TreeNode*subroot){
+        if(root==NULL&&subroot==NULL)return true;
+        if(root==NULL||subroot==NULL)return false;
+        if(root->val!=subroot->val)return false;
+        
+        return check(root->left, subroot->left) && check(root->right, subroot->right); 
+    }
+    bool dfs(TreeNode* root, TreeNode* subroot){
+        if(root==NULL||subroot==NULL)return false;
+        if(root->val == subroot->val){
+            return check(root, subroot) || dfs(root->left, subroot) || dfs(root->right, subroot);
+        }
+        return dfs(root->left, subroot) || dfs(root->right, subroot);
+    }
+    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
+        return dfs(root, subRoot);
+    }
+};
