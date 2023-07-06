ee92f627d19e66a4a43ad7829a541bd98a80191d
shangrex <rexhuang881019@gmail.com>
2023-07-06 12:39:28 +0800

    Time: 20 ms (51.70%), Space: 13.7 MB (32.84%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -6,17 +6,27 @@ struct cmp{
 };
 class Solution {
 public:
+    /*
+    Approach 1: priority queue
+    O(NlgN)
+    Approach 2: bucket sort
+    Time: O(N)
+    Space: 
+    Approach 3: selection algorithmns
+    Time: O(k*N)
+    */
     vector<int> topKFrequent(vector<int>& nums, int k) {
         
         vector<int>rst;
         
         map<int, int>fq;
-        
+        // O(N)
         for(int i = 0; i < nums.size(); i++){
             fq[nums[i]] += 1;
         }
         
         priority_queue< pair<int, int>, vector<pair<int, int>>, cmp>pq;
+        // O(NlgN)
         for(auto i = fq.begin(); i != fq.end(); i++){
             pair<int,int>tmp;
             tmp.first = i->first;
@@ -24,6 +34,7 @@ public:
             pq.push(tmp);
         }
         int it = 0;
+        // O(N)
         while(!pq.empty()){
             if(it >= k)break;
             pair<int, int> tmp = pq.top();
