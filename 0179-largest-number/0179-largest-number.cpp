40d3432108a7efb5c77a16f15c2dd79de1884559
shangrex <rexhuang881019@gmail.com>
2023-07-18 16:38:27 +0800

    Time: 15 ms (23.99%), Space: 11.1 MB (86.40%) - LeetHub

---
+++ 0179-largest-number/0179-largest-number.cpp
@@ -0,0 +1,21 @@
+bool comp(int a, int b){
+    string s_a = to_string(a);
+    string s_b = to_string(b);
+    string ab = s_a+s_b;
+    string ba = s_b+s_a;
+    
+    return ab > ba;
+}
+
+class Solution {
+public:
+    string largestNumber(vector<int>& nums) {
+        sort(nums.begin(), nums.end(), comp);
+        string rst = "";
+        for(int i = 0; i < nums.size(); i++){
+            rst += to_string(nums[i]);
+        }
+        if(rst[0] == '0' && rst.length() > 1)return "0";
+        return rst;
+    }
+};
