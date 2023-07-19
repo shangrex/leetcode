eaeecc4dfe64ef78d3aabbbff540b54fdb15d074
shangrex <rexhuang881019@gmail.com>
2023-07-20 00:41:35 +0800

    Time: 28 ms (92.70%), Space: 25 MB (52.57%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    /*
+    approach 1: create a new array and copy the original array to new array by add k index
+    time: O(N)
+    space: O(N)
+    
+    approach 2: rotate 1 in k times
+    time: O(kN)
+    space: O(1)
+    
+    approach 3: Cyclic Replacements
+    time: O(N)
+    space: O(1)
+    */
+    void rotate(vector<int>& nums, int k) {
+        k = k%nums.size();
+        reverse(nums.begin(), nums.end());
+        reverse(nums.begin(), nums.begin()+k);
+        reverse(nums.begin()+k, nums.end());
+    }
+};
