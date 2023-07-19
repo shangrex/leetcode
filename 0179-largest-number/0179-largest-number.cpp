93d4e1cc52edc95bb78a71029868015dd89ce6d2
shangrex <rexhuang881019@gmail.com>
2023-07-19 21:45:47 +0800

    Time: 19 ms (13.53%), Space: 11 MB (96.52%) - LeetHub

--- 0179-largest-number/0179-largest-number.cpp
+++ 0179-largest-number/0179-largest-number.cpp
@@ -3,8 +3,11 @@ bool comp(int a, int b){
     string s_b = to_string(b);
     string ab = s_a+s_b;
     string ba = s_b+s_a;
-    
-    return ab > ba;
+    for(int i = 0 ; i < ab.length(); i++){
+        if(ab[i] > ba[i])return true;
+        else if(ab[i] < ba[i])return false;
+    }
+    return false;
 }
 
 class Solution {
@@ -15,7 +18,9 @@ public:
         for(int i = 0; i < nums.size(); i++){
             rst += to_string(nums[i]);
         }
+        
         if(rst[0] == '0' && rst.length() > 1)return "0";
+        
         return rst;
     }
 };
