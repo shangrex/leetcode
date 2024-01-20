118d80d416a0c3c14722f5cb8102283bc122ec03
shangrex <rexhuang881019@gmail.com>
2024-01-20 15:23:26 -0600

    Time: 17 ms (71.56%), Space: 26.2 MB (8.59%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
-    vector<int> productExceptSelf(vector<int>& nums) {
-        // be careful division zero
+    /*
+    // be careful division zero
         vector<int>ans(nums.size(), 1);
         long long int suffix = 1;
         // prefix 
@@ -14,5 +14,21 @@ public:
             ans[i-1] = ans[i-1]*suffix;
         }
         return ans;
+    */
+    vector<int> productExceptSelf(vector<int>& nums) {
+        vector<int>rst(nums.size(), 1);
+        long long int suffix = 1;
+        // prifix
+        for(int i = 1 ; i < nums.size(); i++){
+            rst[i] = nums[i-1]*rst[i-1];
+        }
+        // suffix
+        for(int i = nums.size()-1; i > -1; i--){
+            rst[i] = rst[i] * suffix;
+            suffix = nums[i] * suffix;
+        }
+        
+        return rst;
+        
     }
 };
