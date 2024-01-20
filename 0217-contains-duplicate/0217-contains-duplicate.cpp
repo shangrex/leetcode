4c3aa6c652baa5d560ddadf6f1cec3827208fd1a
shangrex <rexhuang881019@gmail.com>
2024-01-20 11:36:15 -0600

    Time: 101 ms (60.16%), Space: 73.1 MB (10.57%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,13 +1,25 @@
 class Solution {
 public:
-    bool containsDuplicate(vector<int>& nums) {
-        unordered_map<int, bool>mp;
+    /*
+    unordered_map<int, bool>mp;
         for(int i = 0; i < nums.size(); i ++){
             // not exist
             if(mp.find(nums[i]) == mp.end()){
                 mp[nums[i]] = true;
             }
             else return true;
+        }
+        return false;
+    */
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int, bool>mp;
+        for(auto i : nums){
+            //exist
+            if(mp.find(i) != mp.end()){
+                return true;
+            }
+            // not exist
+            else mp[i] = true;
         }
         return false;
     }
