84efb1ed907aa2a0f14dec4399665b76d8497e77
shangrex <rexhuang881019@gmail.com>
2024-01-23 21:26:25 -0600

    Time: 8 ms (60.61%), Space: 8.4 MB (5.75%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    bool isAnagram(string s, string t) {
+    /*
         unordered_map<char, int>mp;
         if(s.length() != t.length())return false;
         for(int i = 0; i < s.length(); i++){
@@ -9,6 +9,27 @@ public:
         }
         for(auto i : mp){
             if(i.second != 0)return false;
+        }
+        return true;
+    */
+    /*
+    method 1. count the number or alphabets, then see if stirng t match
+    method 2. 
+    */
+    bool isAnagram(string s, string t) {
+        vector<int> dp(26, 0);
+        // count the number of alpabes in s
+        for(int i = 0; i < s.length(); i++){
+            dp[s[i]-'a'] += 1;
+        }
+        // compare the t and s
+        for(int i = 0; i < t.length(); i++){
+            dp[t[i]-'a'] -= 1;
+        }
+        
+        // check dp
+        for(int i = 0; i < 26; i++){
+            if(dp[i] != 0)return false;
         }
         return true;
     }
