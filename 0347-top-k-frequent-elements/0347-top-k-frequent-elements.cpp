3d2c337f1cf53ac4827409c6bc7e8b0571864390
shangrex <hshangrex@gmail.com>
2024-01-24 23:12:38 -0600

    Time: 10 ms (82.22%), Space: 18 MB (6.03%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -44,7 +44,7 @@ public:
         }
         return rst;
     */
-    vector<int> topKFrequent(vector<int>& nums, int k) {
+    /*
         map<int, int>mp;
         
         for(int i = 0; i < nums.size(); i++){
@@ -62,6 +62,30 @@ public:
             pq.pop();
         }
         return rst;
+    */
+    vector<int> topKFrequent(vector<int>& nums, int k) {
         
+        map<int, int>mp;
+        
+        for(int i = 0; i < nums.size(); i++){
+            mp[nums[i]] += 1;
+        }
+        
+        vector< vector<int>> buckets(nums.size()+1);
+        for(auto i : mp){
+            buckets[i.second].push_back(i.first);
+        }
+        
+        vector<int> rst;
+        int count_k=0;
+        for(int i = buckets.size()-1; i >= 0; i--){
+            if(count_k==k)break;
+            for(auto j : buckets[i]){
+                count_k++;
+                rst.push_back(buckets[i].back());
+                buckets[i].pop_back();
+            }
+        }
+        return rst;
     }
 };
