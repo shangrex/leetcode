552cb13f681e4b5332a87644c19a41e9c9460ce6
shangrex <hshangrex@gmail.com>
2024-02-01 21:27:16 -0600

    Time: 52 ms (98.39%), Space: 26.9 MB (52.57%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    vector<vector<int>> threeSum(vector<int>& nums) {
+    /*
         vector< vector<int>> rst;
         int target;
         sort(nums.begin(), nums.end());
@@ -19,6 +19,33 @@ public:
             i++;
             while(nums[i] == nums[i-1] && i < nums.size()-2)i++;
         }
+        return rst;
+    */
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> rst;
+        sort(nums.begin(), nums.end());
+        
+        for(int i = 0; i < nums.size(); i++){
+            while(i>0 && i < nums.size()-2 && nums[i] == nums[i-1])i++;
+            int l = i+1, r = nums.size()-1;
+
+            while(l < r){
+                if(nums[i] + nums[l] + nums[r] > 0){
+                    r--;
+                    while(l<r && nums[r] == nums[r+1])r--;
+                }
+                else if(nums[i] + nums[l] + nums[r] < 0){
+                    l++;
+                    while(l<r && nums[l] == nums[l-1])l++;
+                }
+                else {
+                    rst.push_back({nums[i], nums[l], nums[r]});
+                    l++;
+                    while(l<r && nums[l] == nums[l-1])l++;
+                }
+            }
+        }
+        
         return rst;
     }
 };
