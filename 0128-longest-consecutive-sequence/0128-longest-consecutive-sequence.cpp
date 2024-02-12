8e60c3454a28f055cf89f077e78771847905060d
shangrex <hshangrex@gmail.com>
2024-02-12 13:02:36 +0800

    Time: 218 ms (37.22%), Space: 99.3 MB (5.01%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -1,72 +1,41 @@
 class Solution {
-/*
-method 1: union find
-the head of the union is the smallest number of the set, and the set can connect if the element's value difference is 1
-
-method 2: set, 
-add all element into set, and used hashtable to check the longest continuouse sequence
-*/    
+public:
     
-/*
-        // initialize the union set
+    unordered_map<int, int> mp;
+    unordered_map<int, int> mp_size;
+    
+    int find(int a){
+        if(mp[a] == a)return a;
+        return find(mp[a]);
+    }
+    
+    void Union(int a, int b){
+        // a = b-1
+        int head_a = find(a);
+        int head_b = find(b);
+        
+        mp[head_b] = head_a;
+        mp_size[head_a] += mp_size[head_b];
+    }
+    
+    int longestConsecutive(vector<int>& nums) {
+        int rst = 0;
+        
         for(int i = 0; i < nums.size(); i++){
-            init(nums[i]);
+            mp[nums[i]] = nums[i];
+            mp_size[nums[i]] = 1;
         }
+        
         for(auto i : mp){
-            // exist value - 1
-            if(mp.count(i.first-1) != 0){
-                Union(i.first, i.first-1);
+            if(mp.find(i.second-1) != mp.end()){
+                Union(i.second-1, i.second);
             }
         }
-        int rst = 0;
+        
         for(auto i : mp_size){
             rst = max(rst, i.second);
         }
         
-        return rst;
-*/
-public:
-    // union set data structure
-    unordered_map<int, int> mp;
-    // count the set size
-    unordered_map<int, int> mp_size;
-    //initialize the union set
-    void init(int value){
-        mp[value] = value;
-        mp_size[value] = 1;
-    }
-    
-    // union the two set 
-    void Union(int a, int b){
-        // a  b=(a-1)
-        int head_a = Find(a);
-        int head_b = Find(b);
-        mp[head_a] = head_b;
-        mp_size[head_b] += mp_size[head_a];
-    }
-    
-    // return the head of the union set
-    int Find(int value){
-        if(mp[value] == value) return value;
-        return Find(mp[value]);
-    }
-    
-    int longestConsecutive(vector<int>& nums) {
-        // init
-        for(int i = 0; i < nums.size(); i++){
-            init(nums[i]);
-        }
-        // union
-        for(auto i : mp){
-            if( mp.find(i.first-1) != mp.end()){
-                Union(i.first, i.first-1);
-            }
-        }
-        int rst=0;
-        // count size
-        for(auto i : mp_size){
-            if(i.second > rst)rst = i.second;
-        }
         return rst;
     }
 };
