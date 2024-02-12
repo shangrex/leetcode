6501e9d853868d063dbfb5db2417173a06ec4f99
shangrex <hshangrex@gmail.com>
2024-02-13 06:58:32 +0800

    Time: 6 ms (20.25%), Space: 9.8 MB (17.96%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,8 +1,12 @@
 class Solution {
 public:
-    
-    
-    void backtrack(vector<int>&nums, vector<int>&pool, vector<vector<int>>& rst){
+    /*
+        method 1. to optimize the find function in backtrack, we need to use another vector used to store whether already in pool or not.
+        draw the selection from the pool
+        method 2.
+        swap the number in nums
+    */
+    void backtrack(vector<int>&nums, vector<int>&pool, vector<bool>&used, vector<vector<int>>& rst){
 
         if(pool.size() == nums.size()){
             rst.push_back(pool);
@@ -10,10 +14,12 @@ public:
         }
         
         for(int i = 0; i < nums.size(); i++){
-            if(find(pool.begin(), pool.end(), nums[i]) == pool.end()){
+            if(used[i] == false){
+                used[i] = true;
                 pool.push_back(nums[i]);
-                backtrack(nums, pool, rst);
+                backtrack(nums, pool, used, rst);
                 pool.pop_back();
+                used[i] = false;
             }
         }
         return;
@@ -22,7 +28,8 @@ public:
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> rst;
         vector<int>pool;
-        backtrack(nums, pool, rst);
+        vector<bool>used(nums.size(), false);
+        backtrack(nums, pool, used, rst);
         return rst;
     }
 };
