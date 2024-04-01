e4e9b3c230d47b76f49cc4d05fe7cf9cffe536ac
shangrex <hshangrex@gmail.com>
2024-03-31 23:58:51 -0500

    Time: 23 ms (17.20%), Space: 20 MB (33.13%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
-    int change(int amount, vector<int>& coins) {
+    /*
+    method 1. bottom-up solution
+        int change(int amount, vector<int>& coins) {
         vector<int>dp(amount+1, 0);
         dp[0] = 1;
         for(int i = 0; i < coins.size(); i++){
@@ -10,5 +12,34 @@ public:
         }
         
         return dp[amount];
+    }
+    
+    method 2. top-down solution & memorization
+    */
+    
+    public:
+    vector<vector<int>>memo;
+    vector<int>coins;
+    int dp(int amount, int index){
+        // cout << amount << " " << index << endl;
+        if(amount == 0)return 1;
+        if(index == coins.size() || amount < 0)return 0;
+        if(memo[index][amount] != -1)return memo[index][amount]; 
+        // if(coins[index] > amount)return 0;
+        
+        // select coins[index]
+        int sum1 = dp(amount - coins[index], index);
+        // not select
+        int sum2 = dp(amount, index+1);
+        
+        memo[index][amount] = sum1+sum2;
+        return memo[index][amount];
+    }
+    
+    int change(int amount, vector<int>& coins) {
+        memo.assign(coins.size(), vector<int>(amount+1, -1));
+        this->coins = coins;
+        return dp(amount, 0);
+        
     }
 };
