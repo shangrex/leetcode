071a51e2bc57f4a285b3732ef692eacee6d95a7f
shangrex <hshangrex@gmail.com>
2024-03-31 22:47:15 -0500

    Time: 5 ms (78.80%), Space: 8.4 MB (87.95%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -2,14 +2,6 @@ class Solution {
 public:
     int change(int amount, vector<int>& coins) {
         vector<int>dp(amount+1, 0);
-        
-        dp[0] = 1;
-        
-        // for(int i = 1; i <= amount; i++){
-        //     for(int j = 0; j < coins.size(); j++){
-        //         if(i-coins[j] >= 0)dp[i] += dp[i-coins[j]];
-        //     }
-        // }
         dp[0] = 1;
         for(int i = 0; i < coins.size(); i++){
             for(int j = coins[i]; j <= amount; j++){
