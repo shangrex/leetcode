70ceeb1ca79ca22d74f834515c5847a8ed4397de
shangrex <hshangrex@gmail.com>
2024-04-06 22:06:37 -0500

    Time: 4 ms (32.65%), Space: 7.3 MB (19.79%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
-    int hammingWeight(uint32_t n) {
-        int rst=0;
+    int hammingWeight(int n) {
+        int rst = 0;
         while(n){
-            if(n&1)rst++;
-            n>>=1;
+            if(n&0x01 == 1)rst++;
+            n = n >> 1;
         }
         return rst;
     }
