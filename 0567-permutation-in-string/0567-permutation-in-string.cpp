99bbb5e44003f18ce17e1f9b6cc2e9f45ffc1f10
shangrex <hshangrex@gmail.com>
2024-04-07 15:47:03 -0500

    Time: 8 ms (59.92%), Space: 8.6 MB (60.10%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -10,7 +10,7 @@ public:
     
     bool checkInclusion(string s1, string s2) {
         if(s1.size() > s2.size())return false;
-        int left = 0, right = -1;
+        int left = 0, right = 0;
         
         vector<int>count_s1(26, 0);
         vector<int>count_s2(26, 0);
@@ -21,19 +21,18 @@ public:
         
         int n2 = s2.length();   
         
-        while(right < n2-1){
-            right++;
+        while(right < n2){
             count_s2[s2[right]-'a']++;
             while(right-left+1 < n1){
                 right++;
                 count_s2[s2[right]-'a']++;
             }      
             
-            // cout << right << " " << left << endl;
             if(match(count_s1, count_s2))return true;
             
             count_s2[s2[left]-'a']--;
             left++;
+            right++;
 
         }
         return false;
