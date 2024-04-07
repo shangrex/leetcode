548b7a481e39b12d9db352d14e380fdcba5978e5
shangrex <hshangrex@gmail.com>
2024-04-06 21:03:24 -0500

    Time: 7 ms (75.31%), Space: 8.2 MB (83.31%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,30 +1,27 @@
 class Solution {
 public:
-    /*
-    approach 1
-    The problem says that we can make at most k changes to the string (any character can be replaced with any other character). So, let's say there were no constraints like the k. Given a string convert it to a string with all same characters with minimal changes. The answer to this is
-
-length of the entire string - number of times of the maximum occurring character in the string
-
-Given this, we can apply the at most k changes constraint and maintain a sliding window such that
-
-(length of substring - number of times of the maximum occurring character in the substring) <= k
-    */
     int characterReplacement(string s, int k) {
-        int rst = 0, max_f = 0, right = 0, left = 0;
-        char tmp;
-        unordered_map<char, int>mp;
-        for(int right = 0; right < s.length(); right++){
-            // right bound move
-            mp[s[right]]++;
-            // find the most frequency
-            if(max_f < mp[s[right]])max_f = mp[s[right]];
-            // left bound move
-            if(right-left-max_f+1 > k){
-                mp[s[left++]]--;
+        int left = 0, right = -1;
+        vector<int>count(26, 0);
+        // the minimal changes means to use max_char_count
+        int max_char_count = 0;
+        int rst = 0;
+        // awaare of comparison of signed int and unsigned int
+        int n = s.length();
+        
+        while(right < n-1){
+            right++;
+            count[s[right]-'A']++;
+            for(auto i : count)max_char_count = max(max_char_count, i);
+            
+            while(right-left+1-max_char_count > k){
+                count[s[left]-'A']--;
+                left++;
+                for(auto i : count)max_char_count = max(max_char_count, i);
+                
             }
-            // record the result
-            rst = max(rst, right-left+1);
+            
+            rst = max(rst, right - left + 1);
         }
         return rst;
     }
