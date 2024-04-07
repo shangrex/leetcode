5a2ac4217c4c9a8f7e5a3f8d96e3fa8f76148b48
shangrex <hshangrex@gmail.com>
2024-04-06 22:18:15 -0500

    Time: 2 ms (74.64%), Space: 8.9 MB (80.86%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,15 +1,10 @@
 class Solution {
 public:
     vector<int> countBits(int n) {
-        vector<int>dp(n+1);
-        if(n==0)return {0};
-        if(n==1)return {0, 1};
-        dp[0] = 0;
-        dp[1] = 1;
-        for(int i = 1;i <= n/2; i++){
-            if(i*2<=n)dp[i*2] = dp[i];
-            if(i*2+1<=n)dp[i*2+1] = dp[i]+1;
+        vector<int>rst(n+1, 0);
+        for(int i = 0; i <= n; i++){
+            rst[i] = __builtin_popcount(i);
         }
-        return dp;
+        return rst;
     }
 };
