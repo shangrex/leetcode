afc8dc2c96398a53b56d201936f30953c1b98bbf
shangrex <hshangrex@gmail.com>
2024-04-07 09:34:31 -0500

    Time: 0 ms (100.00%), Space: 7.1 MB (29.07%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.cpp
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -1,11 +1,17 @@
 class Solution {
 public:
     uint32_t reverseBits(uint32_t n) {
-        uint32_t  tmp = 0x80000000, rst=0;
+        uint32_t rst = 0;
+        
+        uint32_t mask = 1;
         for(int i = 0; i < 32; i++){
-            if(n&tmp)rst|=1<<i;
-            tmp = tmp >> 1;
+            if(mask & n){
+                rst |= 1;
+            }
+            if(i<31)rst = rst << 1;
+            mask  = mask << 1;
         }
+        
         return rst;
-    }
-};
+    }                                  
+};                    
