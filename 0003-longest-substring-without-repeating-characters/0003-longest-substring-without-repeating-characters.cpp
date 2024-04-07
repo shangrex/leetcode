39e438166090afbc0ee472a116945eb310fd224c
shangrex <hshangrex@gmail.com>
2024-04-06 19:05:48 -0500

    Time: 16 ms (52.85%), Space: 13.5 MB (38.81%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,27 +1,21 @@
 class Solution {
 public:
-    /*
-    approach 1 sliding windows
-    optimize method 2 record the left boundary
-    */
     int lengthOfLongestSubstring(string s) {
-  unordered_map<char, int> substr; // char, count
-  int left = 0, right = 0;
-  int ans = 0;
-  while(right < s.size()) {
-    char c = s[right];
-    right++;
-    substr[c]++;
-    
-    // make sure it is valid
-    while(substr[c] > 1) {
-      char c2 = s[left];
-      substr[c2]--;
-      left++;
-    }
-    
-    ans = max(ans, right - left);
-  }
-  return ans;
+        int left = 0, right = 0;
+        unordered_map<char, int>mp;
+        int rst = 0;
+        while(right < s.length()){
+            if(mp.find(s[right]) == mp.end()){
+                mp[s[right]] = 1;
+                rst =  max(rst, right - left+1);
+                right++;
+            }
+            else {
+                mp.erase(s[left]);
+                left++;
+            }
+        }
+        
+        return rst;
     }
 };
