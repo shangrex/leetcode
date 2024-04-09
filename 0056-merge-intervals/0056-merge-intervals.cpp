1062f1d4498492b7a057060eb89438dddc063472
shangrex <hshangrex@gmail.com>
2024-04-09 11:56:10 -0500

    Time: 21 ms (73.79%), Space: 22.7 MB (41.71%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,26 +1,27 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        vector<vector<int>> rst;
-        if(intervals.size() == 0)return rst;
-        
-        sort(intervals.begin(), intervals.end(), [](const vector<int>&a, const vector<int>&b){
+        sort(intervals.begin(), intervals.end(), [](const vector<int> &a, const vector<int> &b){
             return a[0] < b[0];
         });
+        int left = intervals[0][0], right = intervals[0][1];
         
-        int tmp_head = intervals[0][0], tmp_tail = intervals[0][1];
-        for(int i = 1; i < intervals.size(); i++){
-            if(tmp_tail >= intervals[i][0]){
-                tmp_tail = max(intervals[i][1], tmp_tail);
-                continue;
+        
+        // for(auto i : intervals)cout << i[0] << endl;
+        vector<vector<int>> rst;
+        
+        for(int i = 0; i < intervals.size(); i++){
+            if(intervals[i][0] <= right){
+                right = max(right, intervals[i][1]);
             }
-            else {
-                rst.push_back({tmp_head, tmp_tail});
-                tmp_head = intervals[i][0];
-                tmp_tail = intervals[i][1];
+            else if(intervals[i][0] > right){
+                rst.push_back({left, right});
+                left = intervals[i][0];
+                right = intervals[i][1];
             }
         }
-        rst.push_back({tmp_head, tmp_tail});
+        rst.push_back({left, right});
+        
         return rst;
     }
 };
