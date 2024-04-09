83c0dca42f37fe165d2cdde2a3d4d0ab093b4843
shangrex <hshangrex@gmail.com>
2024-04-09 11:20:22 -0500

    Time: 11 ms (61.86%), Space: 20.7 MB (13.94%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -1,66 +1,42 @@
 class Solution {
 public:
-    /*
-        vector<vector<int>> rst;
-        int i = 0;
-        // find the fist section of the rst
-        for(; i < intervals.size(); i++){
-            // non overlapping
-            if(newInterval[0] > intervals[i][1]){
-                rst.push_back({intervals[i]});
-            }
-            else break;
-        }
-        
-        int tmp_head = newInterval[0], tmp_tail = newInterval[1];
-        for(; i < intervals.size(); i++){
-            tmp_head = min(intervals[i][0], tmp_head);
-            // overlapping
-            if(newInterval[1] >= intervals[i][0]){
-                tmp_tail = max(tmp_tail, intervals[i][1]);
-            }
-            else break;
-        }
-        rst.push_back({tmp_head, tmp_tail});
-        
-        for(; i < intervals.size(); i++){
-            // non overlapping
-            if(newInterval[1] < intervals[i][0]){
-                rst.push_back({intervals[i]});
-            }
-            else break;
-        }
-        
-
-        return rst;
-    */
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-        vector<vector<int>> rst;
-        int i = 0;
+        if(intervals.size() == 0){
+            return {newInterval};
+        }
+        int left = newInterval[0], right = newInterval[1];
         
-        // add non overlappint part into head of rst
-        while(i < intervals.size()){
+        vector<vector<int>>rst;
+        if(newInterval[1] < intervals[0][0]){
+            rst.push_back({newInterval});
+            left = intervals[0][0];
+            right = intervals[0][1];
+        }
+        
+        int i = 0;        
+        for(;i < intervals.size(); i++){
             if(intervals[i][1] < newInterval[0]){
                 rst.push_back(intervals[i]);
-                i++;
             }
             else break;
         }
         
-        // add new Interval into rst
-        
-        while( i < intervals.size() && newInterval[1] >= intervals[i][0] && newInterval[0] <= intervals[i][1]){
-            newInterval[0] = min(newInterval[0], intervals[i][0]);
-            newInterval[1] = max(newInterval[1], intervals[i][1]);
-            i++;
+        for(; i < intervals.size(); i++){
+            cout << left << " " << right << endl;
+            if(intervals[i][1] >= newInterval[0] && intervals[i][0] <= newInterval[1]){
+                // overlape
+                left = min(left, intervals[i][0]);
+                right = max(newInterval[1], intervals[i][1]);
+            }
+            else if(intervals[i][0] > right){
+                rst.push_back({left, right});
+                left = intervals[i][0];
+                right = intervals[i][1];
+            }
         }
-        rst.push_back(newInterval);
+        rst.push_back({left, right});
         
-        // add non overlapping part into the end of rst
-        while( i < intervals.size()){
-            rst.push_back(intervals[i]);
-            i++;
-        }
+
         
         return rst;
     }
