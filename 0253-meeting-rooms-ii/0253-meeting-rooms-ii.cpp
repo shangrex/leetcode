46533ec29e6c974a4b2aae48fbad40ef27eb4286
shangrex <hshangrex@gmail.com>
2024-04-09 14:02:04 -0500

    Time: 10 ms (77.44%), Space: 15.4 MB (61.91%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.cpp
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.cpp
@@ -1,19 +1,25 @@
-
-    class Solution {
+class Solution {
 public:
     int minMeetingRooms(vector<vector<int>>& intervals) {
         sort(intervals.begin(), intervals.end());
-        priority_queue<int, vector<int>, greater<int>> q;
-        int ans = 1;
-        q.push(intervals[0][1]);
-        for (int i = 1; i < intervals.size(); ++i) {
-            if (intervals[i][0] < q.top()) {
-                ans++;
-            } else {
-                q.pop();
+        priority_queue<int, vector<int>, greater<int>>pq;
+        
+        int rst = 0;
+        for(int i = 0; i < intervals.size(); i++){
+            if(pq.empty()){
+                rst++;
+                pq.push(intervals[i][1]);
             }
-            q.push(intervals[i][1]);
+            else if(intervals[i][0] < pq.top()){
+                pq.push(intervals[i][1]);
+                rst++;
+            }
+            else {
+                pq.pop();
+                pq.push(intervals[i][1]);
+            }
+            
         }
-        return ans;
+        return rst;
     }
 };
