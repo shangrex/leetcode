7306d870c61905bc4f23afcdafb358dc3fb285e5
shangrex <hshangrex@gmail.com>
2024-04-09 12:23:46 -0500

    Time: 16 ms (21.13%), Space: 15.1 MB (29.39%) - LeetHub

--- 0252-meeting-rooms/0252-meeting-rooms.cpp
+++ 0252-meeting-rooms/0252-meeting-rooms.cpp
@@ -2,13 +2,17 @@ class Solution {
 public:
     bool canAttendMeetings(vector<vector<int>>& intervals) {
         sort(intervals.begin(), intervals.end(), [](vector<int>&a, vector<int>&b){
-            return a[0] < b[0];
+            if(a[1] < b[1])return true;
+            else if(a[1] > b[1])return false;
+            else if(a[0] < b[0])return true;
+            else return false;
         });
         
         for(int i = 1; i < intervals.size(); i++){
-            if(intervals[i][0] < intervals[i-1][1])return false;
+            if(intervals[i][0] < intervals[i-1][1]){
+                return false;
+            }
         }
         return true;
-        
     }
 };
