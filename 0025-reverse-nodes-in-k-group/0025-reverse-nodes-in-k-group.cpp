a50fa373727698f5508001cae4b57db5ccfe1dc6
shangrex <hshangrex@gmail.com>
2024-04-10 12:41:20 -0500

    Time: 3 ms (98.98%), Space: 15.1 MB (10.49%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
@@ -7,10 +7,54 @@
  *     ListNode(int x) : val(x), next(nullptr) {}
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
+ 
  */
+// class Solution {
+// public:
+//     ListNode* reverseLinkedList(ListNode* head, int k){
+//         ListNode *prev = NULL;
+//         ListNode *cur = head;
+//         while(k > 0){
+//             ListNode*tmp = cur->next;
+//             cur->next = prev;
+//             prev = cur;
+//             cur = tmp;
+//             k--;
+//         }
+        
+//         return prev;
+//     }
+    
+//     ListNode* reverseKGroup(ListNode* head, int k) {
+//         ListNode *cur = head;
+//         int cnt = 0;
+//         while(cnt < k && cur != NULL){
+//             cur = cur->next;
+//             cnt++;
+//         }
+        
+//         if(cnt == k){
+//             ListNode *rev_cur = reverseLinkedList(head, k);
+//             head->next = reverseKGroup(cur, k);
+//             return rev_cur;
+//         }
+        
+//         return head;
+//     }
+// };
 class Solution {
 public:
+    /*
+    Apporach 1. recursive
+    first step check the list lenght is bigger than k or not 
+        if bigger than return head
+        otherwise return the new node that is the head of reversed linked list
+    second step
+        
     
+    
+    Approach 2. iterative
+    */
     ListNode* reverseLinkedList(ListNode* head, int k){
         ListNode *prev = NULL;
         ListNode *cur = head;
@@ -34,9 +78,16 @@ public:
         }
         
         if(cnt == k){
-            ListNode *rev_cur = reverseLinkedList(head, k);
+            ListNode* prev = NULL, *cur = head, *tmp_next;
+            for(int i = 0; i < k; i++){
+                tmp_next = cur->next;
+                cur->next = prev;
+                prev = cur;
+                cur = tmp_next;
+            }
+            
             head->next = reverseKGroup(cur, k);
-            return rev_cur;
+            return prev;
         }
         
         return head;
