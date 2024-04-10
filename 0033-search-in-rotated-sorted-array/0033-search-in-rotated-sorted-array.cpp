cf26f4826acf602a574d49b7bf48d55a9ee4a3f7
shangrex <hshangrex@gmail.com>
2024-04-10 11:17:49 -0500

    Time: 3 ms (61.46%), Space: 13.6 MB (5.75%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,35 +1,37 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int left = 0, right = nums.size()-1, mid;
-        while(left <= right){
-            mid = (left+right)/2;
-            if(nums[mid] == target)return mid;
-            if(left==right)break;
-            // right is sorted
-            if(nums[mid] < nums[right]){
-                // target in right
-                if(nums[mid] < target && target <= nums[right]){
-                    left = mid + 1;
-                }
-                // target in left
-                else {
-                    right = mid;
-                }
+        
+        if(nums.size() == 1 && nums[0] == target) return 0;
+        int left = 0, right = nums.size();
+        
+        while(left < right){
+            int mid = (left+right)/2;
+            // cout << left << ' ' << mid << ' ' << right << endl;
+            if(nums[mid] == target){
+                return mid;
             }
-            // left is sorted
-            else {
-                // target in left
-                if(nums[left] <= target && target < nums[mid]){
+            else if(nums[mid] > nums[right-1]){
+                // left is asceding
+                if(target >= nums[left] && target < nums[mid]){
+                    // target is in left split
                     right = mid;
                 }
-                // target in right
                 else {
                     left = mid+1;
                 }
             }
+            else {
+                // right is in asceding
+                if(nums[right-1] >= target && nums[mid] < target){
+                    // target is in right split
+                    left = mid+1;
+                }
+                else {
+                    right = mid;
+                }
+            }
         }
-        // not found
         return -1;
     }
 };
