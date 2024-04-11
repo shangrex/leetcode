d418d86820e417ab651faf8aa26b08253409c680
shangrex <hshangrex@gmail.com>
2024-04-11 11:21:43 -0500

    Time: 3 ms (46.60%), Space: 14.8 MB (5.36%) - LeetHub

--- 0274-h-index/0274-h-index.cpp
+++ 0274-h-index/0274-h-index.cpp
@@ -1,5 +1,9 @@
 class Solution {
 public:
+    /*
+    Approach 1. after sorting, the key(index) of the citations can be seen as
+    the number of larget rst
+    so to get the number of citations which is larget than rst is citations[n_c - rst]
     int hIndex(vector<int>& citations) {
         sort(citations.begin(), citations.end());
         int rst = 1;
@@ -9,7 +13,26 @@ public:
             rst++;
         }
         
-        
         return rst-1;
+    }
+    
+    Approach 2. use couting to record the number of citations which is larget than rst
+    */
+    int hIndex(vector<int>& citations) {
+        int rst = 1;
+        int n_c = citations.size();
+        vector<int>count(10100, 0);
+        
+        for(int i = 0; i < citations.size(); i++){
+            count[min(n_c, citations[i])] ++;
+        }
+        
+        int cnt = 0;
+        for(int i = n_c; i >=0; i--){
+            cnt += count[i];
+            if(cnt >= i){rst = i;break;}
+        }
+        
+        return rst;
     }
 };
