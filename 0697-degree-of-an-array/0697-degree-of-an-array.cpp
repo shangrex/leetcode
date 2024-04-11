f70fbceea8a010771bd40f2514a57048549d2052
shangrex <hshangrex@gmail.com>
2024-04-10 22:50:56 -0500

    Time: 28 ms (63.63%), Space: 29.7 MB (27.26%) - LeetHub

--- 0697-degree-of-an-array/0697-degree-of-an-array.cpp
+++ 0697-degree-of-an-array/0697-degree-of-an-array.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
     int findShortestSubArray(vector<int>& nums) {
-        unordered_map<int, int>left, count, freq;
+        unordered_map<int, int>left, right, freq;
         
         for(int i = 0; i < nums.size(); i++){
-            if(left.find(nums[i]) == left.end()){left[nums[i]] = i;count[nums[i]] = 1;}
-            else count[nums[i]] = i-left[nums[i]]+1;
+            if(left.find(nums[i]) == left.end()){left[nums[i]] = i;}
+            // else count[nums[i]] = i-left[nums[i]]+1;
+            right[nums[i]] = i;
             if(freq.find(nums[i]) != freq.end())freq[nums[i]]++;
             else freq[nums[i]] = 1;
         }
@@ -20,7 +21,7 @@ public:
         for(auto i : freq){
             if(i.second == max_freq){
                 // cout << i.first;
-                rst = min(rst, count[i.first]);
+                rst = min(rst, right[i.first] - left[i.first] + 1);
             }
         }
         
