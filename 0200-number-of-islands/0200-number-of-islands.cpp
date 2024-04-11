ae0b24ac5dd3037147ee059fb8b9d3de6fcb9dfb
shangrex <hshangrex@gmail.com>
2024-04-10 23:11:05 -0500

    Time: 26 ms (77.10%), Space: 15.8 MB (66.83%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,51 +1,39 @@
 class Solution {
 public:
+    
+    void dfs(vector<vector<char>> &grid, int i, int j){
+        grid[i][j] = '0';
+        int row = grid.size();
+        int col = grid[0].size();
+        
+        if(i-1 >= 0 && grid[i-1][j] == '1'){
+            dfs(grid, i-1, j);
+        }
+        if(i+1 < row && grid[i+1][j] == '1'){
+            dfs(grid, i+1, j);
+        }
+        if(j-1 >= 0 && grid[i][j-1] == '1'){
+            dfs(grid, i, j-1);
+        }
+        if(j+1 < col && grid[i][j+1] == '1'){
+            dfs(grid, i, j+1);
+        }
+        
+        
+    }
+    
     int numIslands(vector<vector<char>>& grid) {
-        if(grid.size() == 0)return 0;
-        int n = grid.size();
-        int m = grid[0].size();
-        // 0 means not visited
-        // 1 means visited
-        vector<vector<int> >visited(n, vector<int>(m, 0));
-        int rst = 0;
-        for(int i = 0;i < n; i++){
-            for(int j = 0; j < m; j++){
-                if(visited[i][j] == 0 && grid[i][j] == '1'){
-                    rst++;
-                    dfs(i, j, visited, grid);
+        int row = grid.size();
+        int col = grid[0].size();
+        int cnt = 0;
+        for(int i = 0; i < row; i++){
+            for(int j = 0; j < col; j++){
+                if(grid[i][j] == '1'){
+                    cnt++;
+                    dfs(grid, i, j);
                 }
             }
         }
-        return rst;
-    }
-    
-    void dfs(int row, int col, vector<vector<int>> &visited, vector<vector<char>>& grid){
-        int n = grid.size();
-        int m = grid[0].size();
-        visited[row][col] = 1;
-        // down
-        if(row+1 < n){
-            if(grid[row+1][col] == '1' && visited[row+1][col] == 0){
-                dfs(row+1, col, visited, grid);
-            }            
-        }
-        // up
-        if(row-1 >= 0){
-            if(grid[row-1][col] == '1' && visited[row-1][col] == 0){
-                dfs(row-1, col, visited, grid);
-            }
-        }
-        // left
-        if(col-1 >= 0){
-            if(grid[row][col-1] == '1' && visited[row][col-1] == 0){
-                dfs(row, col-1, visited, grid);
-            }
-        }
-        // right
-        if(col+1 < m){
-            if(grid[row][col+1] == '1' && visited[row][col+1] == 0){
-                dfs(row, col+1, visited, grid);
-            }
-        }
+        return cnt;
     }
 };
