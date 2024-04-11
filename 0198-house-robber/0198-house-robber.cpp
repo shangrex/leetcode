e349f949613de90c03b9c834c5041ae69eb6c1d0
shangrex <hshangrex@gmail.com>
2024-04-11 12:23:40 -0500

    Time: 4 ms (32.68%), Space: 9.3 MB (33.54%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,14 +1,11 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        if(nums.size() == 1)return nums[0];
-        // pre means i-2 maximun, cur means i-1 maxixmum
-        int pre = 0, cur = nums[0], tmp;
-        for(int i = 1; i < nums.size(); i++){
-            tmp = max(pre+nums[i], cur);
-            pre = cur;
-            cur = tmp;
+        vector<int>dp(nums.size()+1, 0);
+        dp[1] = nums[0];
+        for(int i = 2; i <= nums.size(); i++){
+            dp[i] = max(dp[i-1], dp[i-2]+nums[i-1]);
         }
-        return cur;
+        return dp[nums.size()];
     }
 };
