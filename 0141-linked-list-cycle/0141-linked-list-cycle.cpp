d42eb8d4e51ef67f0efeafcbbe830530b579e6c9
shangrex <hshangrex@gmail.com>
2024-04-24 00:51:52 +0800

    Time: 7 ms (83.55%), Space: 10.6 MB (61.52%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -9,15 +9,15 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        ListNode *slow = head, *fast = head;
-        
-        while(fast != NULL){
+        ListNode *slow = head;
+        ListNode *fast = head;
+        while(fast && fast->next){
             slow = slow->next;
-            if(fast->next != NULL)fast = fast->next->next;
-            else break;
-            if(fast == slow)return true;
+            fast = fast->next->next;
+            if(slow == fast){
+                return true;
+            }
         }
         return false;
-        
     }
 };
