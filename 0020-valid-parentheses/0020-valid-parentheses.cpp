e56590fc50955b3ca33191d0ade67f38953b6a23
shangrex <hshangrex@gmail.com>
2024-05-07 14:04:34 -0500

    Time: 0 ms (100.00%), Space: 7.6 MB (59.64%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,26 +1,32 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char>check;
+        stack<char> st;
+        
         for(int i = 0; i < s.length(); i++){
-            if(s[i] == '(' || s[i] == '{' || s[i] == '['){
-                check.push(s[i]);
-                continue;
-            }
-            if(s[i] == ')'){
-                if(!check.empty() && check.top() == '(')check.pop(); 
+            if(!st.empty() && s[i] == ')'){
+                if(st.top() == '('){
+                    st.pop();
+                }
                 else return false;
             }
-            else if(s[i] == '}'){
-                if(!check.empty() && check.top() == '{')check.pop(); 
+            else if(!st.empty() && s[i] == ']'){
+                if(st.top() == '['){
+                    st.pop();
+                }
                 else return false;
             }
-            else if(s[i] == ']'){
-                if(!check.empty() && check.top() == '[')check.pop(); 
+            else if(!st.empty() && s[i] == '}'){
+                if(st.top() == '{'){
+                    st.pop();
+                }
                 else return false;
+            }
+            else {
+                st.push(s[i]);
             }
         }
-        if(!check.empty())return false;
+        if(!st.empty())return false;
         return true;
     }
 };
