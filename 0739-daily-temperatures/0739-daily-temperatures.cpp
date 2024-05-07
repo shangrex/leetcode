a8382836b0374f441bf7c1ba071b7b7fcdec84b7
shangrex <hshangrex@gmail.com>
2024-05-07 18:04:01 -0500

    Time: 122 ms (54.75%), Space: 105.3 MB (60.88%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -5,20 +5,16 @@ public:
     Monotonic stack store the decreasing order of the number 
     */
     vector<int> dailyTemperatures(vector<int>& temperatures) {
-        
         vector<int>rst(temperatures.size(), 0);
         stack<int>st;
-        for(int i = 0; i < temperatures.size(); i++){
-            if(st.empty()){
-                st.push(i);
+        st.push(0);
+        for(int i = 1; i < temperatures.size(); i++){
+            while(!st.empty() && temperatures[st.top()] < temperatures[i]){
+                // warmer
+                rst[st.top()] = i - st.top();
+                st.pop();
             }
-            else {
-                while(!st.empty() && temperatures[st.top()] < temperatures[i]){
-                    rst[st.top()] = i - st.top();                        
-                    st.pop();
-                }
-                st.push(i);
-            }
+            st.push(i);
         }
         return rst;
         
