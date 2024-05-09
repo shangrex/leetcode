466ee926cca0feef3a3ac2d48bd0a0ad76449de6
shangrex <hshangrex@gmail.com>
2024-05-09 14:45:52 -0500

    Time: 19 ms (41.29%), Space: 11.8 MB (41.10%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -5,17 +5,20 @@ public:
         unordered_map<char, int>mp;
         int rst = 0;
         while(right < s.length()){
-            if(mp.find(s[right]) == mp.end()){
-                mp[s[right]] = 1;
-                rst =  max(rst, right - left+1);
-                right++;
+            if(mp.find(s[right]) != mp.end()){
+                mp[s[right]]++;
             }
-            else {
-                mp.erase(s[left]);
+            else mp[s[right]] = 1;
+            
+            while(mp[s[right]] > 1){
+                if(mp[s[left]] == 1)mp.erase(s[left]);
+                else mp[s[left]]--;
                 left++;
             }
+            // cout << left << " " << right << endl;
+            rst = max(rst, (right-left+1));
+            right++;
         }
-        
         return rst;
     }
 };
