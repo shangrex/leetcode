8f6baf2530a22af0382056b80422a0943e8eb215
shangrex <hshangrex@gmail.com>
2024-05-22 18:47:28 -0500

    Time: 14 ms (56.17%), Space: 20.3 MB (56.69%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,24 +1,16 @@
 class Solution {
-    // method 1 summarization
-    /*
-        int missingNumber(vector<int>& nums) {
-        int n = nums.size(), sum_num, tmp=0;
-        sum_num = (n*n+n)/2;
-        for(int i = 0; i < nums.size(); i++){
-            tmp += nums[i];
-        }
-        return sum_num - tmp;
-    }
-    */
-    // method 2 xor
 public:
+    /*
+    Approach 1. Guass 
+    Approach 2. XOR with iterator
+    */
     int missingNumber(vector<int>& nums) {
-        int tmp = 0;
+        int rst = 0;
         for(int i = 0; i < nums.size(); i++){
-            tmp ^= i;
-            tmp ^= nums[i];
+            rst ^= i;
+            rst ^= nums[i];
         }
-        tmp ^= nums.size();
-        return tmp;
+        rst ^= nums.size();
+        return rst;
     }
 };
