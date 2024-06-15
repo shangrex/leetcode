5a5c9909b1bf06ce2b1f4182799a9cdcc98fb43c
shangrex <hshangrex@gmail.com>
2024-06-16 00:34:09 +0800

    Time: 24 ms (5.35%), Space: 9.5 MB (8.46%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -3,8 +3,7 @@ public:
     /*
     Approach 1. DP
     Approach 2. two pointer
-    */
-    bool checkValidString(string s) {
+    
         int left = 0, right = s.length()-1;
         int open = 0, close = 0;
         while(left < s.length()){
@@ -17,6 +16,41 @@ public:
             right--;
         }
         return true;
+    
+    */
+    bool isvalid(vector<vector<int>>&memo,int index,int open, string &s){
+        cout << index << " " << open << endl;
+        if(index == s.length()){
+            if(open == 0)return true;
+            else return false;
+        }
+        if(memo[index][open] != -1) return memo[index][open];
+        bool check = 0;
+        if(s[index] == '*'){
+            // '('
+            check |= isvalid(memo, index+1, open+1, s);
+            // ')'
+            if(open > 0)
+            check |= isvalid(memo, index+1, open-1, s);
+            // ignore
+            check |= isvalid(memo, index+1, open, s);
+        }
+        else if(s[index] == '('){
+            check |= isvalid(memo, index+1, open+1, s);            
+        }
+        else if(s[index] == ')' && open > 0){
+            check |= isvalid(memo, index+1, open-1, s);
+        }
+        
+        memo[index][open] = check;
+        return check;
+        
+    }
+    bool checkValidString(string s) {
+        int n = s.length();
+        vector<vector<int>> memo(n+1, vector<int>(n+1, -1));
+        
+        return isvalid(memo, 0, 0, s);
         
     }
 };
