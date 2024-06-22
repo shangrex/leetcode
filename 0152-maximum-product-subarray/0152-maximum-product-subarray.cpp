5c59e3ea091c4355e82432a45157396fef8ccab6
shangrex <hshangrex@gmail.com>
2024-06-22 08:56:33 +0800

    Time: 3 ms (85.40%), Space: 18 MB (5.15%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -1,17 +1,23 @@
 class Solution {
 public:
+        /*
+    To solve interger overflow problem
+    1. use double or other bigger range type
+    2. control the temp_max and temp temp_min range
+    */
     int maxProduct(vector<int>& nums) {
-        int rst = -10, tmp_min = -10, tmp_max = 10;
-        int tmp;
-        tmp_min = rst = tmp_max = nums[0];
-        for(int i = 1;i < nums.size(); i++){
-            tmp = max({nums[i], nums[i]*tmp_max, nums[i]*tmp_min});
-            rst = max(rst, tmp);
-            // max subarray value
-            tmp = max({1, tmp_max*nums[i], nums[i], tmp_min*nums[i]});
-            // min subarray value
-            tmp_min = min({tmp_min*nums[i], nums[i], tmp_max*nums[i]});
-            tmp_max = tmp;
+        vector<__int128_t>dp_max(nums.size()+10, 0);
+        vector<__int128_t>dp_min(nums.size()+10, 0);
+        
+        dp_max[0] = 1;
+        dp_min[0] = 1;
+        for(int i = 1; i <= nums.size(); i++){
+            dp_max[i] = max({dp_max[i-1]* nums[i-1],(__int128_t) nums[i-1], dp_min[i-1]* nums[i-1]});
+            dp_min[i] = min({(__int128_t) nums[i-1],(__int128_t) dp_min[i-1]*nums[i-1], dp_max[i-1] * nums[i-1]});
+        }
+        __int128_t  rst = nums[0];
+        for(int i = 1; i <= nums.size();i++){
+            rst = max(rst, dp_max[i]);
         }
         return rst;
     }
