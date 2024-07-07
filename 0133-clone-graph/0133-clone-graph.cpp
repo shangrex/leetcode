763289355b3b3b8a00d5bf6d3c84587f43e97049
shangrex <hshangrex@gmail.com>
2024-07-07 11:42:50 +0800

    Time: 4 ms (38.71%), Space: 11.7 MB (31.67%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.cpp
+++ 0133-clone-graph/0133-clone-graph.cpp
@@ -26,17 +26,17 @@ public:
         if(node == NULL){
             return NULL;
         }
-        Node *new_node = new Node(node->val);
-        mp[node] = new_node;
-        visited[node->val] = 1;
+            Node *new_node = new Node(node->val);
+            mp[node] = new_node;
+            visited[node->val] = 1;
         cout << node->val << endl;
-        vector<Node*>tmp;
         for(int i = 0; i < node->neighbors.size(); i++){
+            Node *tmp;
             if(visited[node->neighbors[i]->val] == 0)
-                tmp.push_back(dfs(node->neighbors[i], visited));
-            else tmp.push_back(mp[node->neighbors[i]]);
+                tmp =  dfs(node->neighbors[i], visited);
+            else tmp = mp[node->neighbors[i]];
+            mp[node]->neighbors.push_back(tmp);
         }
-        mp[node]->neighbors = tmp;
         return mp[node];
     }
     Node* cloneGraph(Node* node) {
