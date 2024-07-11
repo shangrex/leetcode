ac41a43c23511f6bbd46394605ab71fbedf3fe2e
shangrex <hshangrex@gmail.com>
2024-07-12 00:00:53 +0800

    Time: 18 ms (30.48%), Space: 27 MB (51.12%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.cpp
+++ 0695-max-area-of-island/0695-max-area-of-island.cpp
@@ -4,14 +4,14 @@ public:
         int h = grid.size();
         int w = grid[0].size();
         grid[i][j] = 0;
-        cout << i << " " << j << endl ;
+        // cout << i << " " << j << endl ;
         int tmp_cnt = cnt;
         cnt++;
         if(i+1 < h && grid[i+1][j] == 1)cnt = dfs(grid, i+1, j, cnt);
         if(j+1 < w && grid[i][j+1] == 1)cnt = dfs(grid, i, j+1, cnt);
         if(i > 0 && grid[i-1][j] == 1)cnt = dfs(grid, i-1, j, cnt);
         if(j > 0 && grid[i][j-1] == 1)cnt = dfs(grid, i, j-1, cnt);
-        cout << "cnt "<< cnt << endl;
+        // cout << "cnt "<< cnt << endl;
         return cnt;
     }
     
@@ -22,7 +22,7 @@ public:
             for(int j = 0; j < grid[0].size(); j++){
                 if(grid[i][j] == 1){
                     rst = max(rst, dfs(grid, i, j, 0));
-                    cout << "rst " << rst << endl;
+                    // cout << "rst " << rst << endl;
                 }
             }
         }
