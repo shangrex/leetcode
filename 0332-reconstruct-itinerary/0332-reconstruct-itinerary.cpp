b63016b7e4c36042b57c05b52699872a63f10206
shangrex <hshangrex@gmail.com>
2024-07-24 09:51:06 +0800

    Time: 14 ms (81.57%), Space: 19.2 MB (51.29%) - LeetHub

---
+++ 0332-reconstruct-itinerary/0332-reconstruct-itinerary.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    void dfs(string cur, unordered_map<string, vector<string>>& graph, vector<string>&rst){
+
+        while(!graph[cur].empty()){
+            string tmp = graph[cur].back();
+            graph[cur].pop_back();
+            dfs(tmp, graph, rst);
+        }
+        rst.push_back(cur);
+        
+        return;
+    }
+    
+    vector<string> findItinerary(vector<vector<string>>& tickets) {
+        vector<string>rst;
+        unordered_map<string, vector<string>> graph;
+        for(auto &ticket : tickets){
+            graph[ticket[0]].push_back(ticket[1]);
+        }
+        
+        for(auto &g : graph){
+            sort(g.second.begin(), g.second.end(), greater<string>());
+        }
+        
+        dfs("JFK", graph, rst);
+        reverse(rst.begin(), rst.end());
+        return rst;
+    }
+};
