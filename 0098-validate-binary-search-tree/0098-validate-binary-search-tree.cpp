4b3801d13bcdccad5fdd87086b8026eb958c0f19
shangrex <hshangrex@gmail.com>
2024-07-24 13:51:15 +0800

    Time: 4 ms (86.84%), Space: 20.4 MB (17.09%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -8,24 +8,18 @@
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
-         34
-        -6 
-    -21 null
  */
 class Solution {
 public:
-    bool check(TreeNode* root, long int left, long int right){
-        if(root == NULL)return true;
-        
-     
-        if(root->val <= left || root->val >= right)return false;
-        
-        return check(root->left, left, root->val) && check(root->right, root->val, right);
-        
+    bool dfs(TreeNode*root, long left, long right){
+        if(!root)return true;
+        //cout << root->val << " " << left << " " << right << endl;
+        if(root->val <= left)return false;
+        if(root->val >= right)return false;
+               
+        return dfs(root->left, left, root->val) && dfs(root->right, root->val, right);
     }
     bool isValidBST(TreeNode* root) {
-        if(root == NULL)return true;
-        // 0 means left, 1 means right
-        return check(root->left, LLONG_MIN, root->val) && check(root->right, root->val, LLONG_MAX	);
+        return dfs(root, LLONG_MIN, LLONG_MAX);
     }
 };
