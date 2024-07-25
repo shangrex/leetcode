b4839beca270cbf4b0e2f1d730ddc03e33a098f6
shangrex <hshangrex@gmail.com>
2024-07-25 17:27:57 +0800

    Time: 22 ms (29.28%), Space: 26.7 MB (15.16%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.cpp
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.cpp
@@ -11,33 +11,19 @@
  */
 class Solution {
 public:
-    /*
-    special technique NULL+3 = 3
-    be aware of sequence feature (dfs can not return left node + right node + middle value) 
-    !! we should be aware of the return value of the dfs
-    */
-    int treesum(TreeNode* root, int &rst){
-        if(root == NULL)return NULL;
-        int left = 0, middle = 0, right = 0;
-        middle = root->val;
-        // the sequence in the left node
-        left = treesum(root->left, rst);
-        
-        // the sequence in the right node
-        right = treesum(root->right, rst);
-        
-        int tmp, seq;
-        // return the sequence value, and the sequence should not repeated so only left or right can choose
-        seq = max({left+middle, middle, right+middle});
-        // choose the biggest value and store to rst
-        tmp = max(seq, left+middle+right);
+    int dfs(TreeNode* root, int &rst){
+        if(!root) return NULL;
+        int middle = root->val;
+        int left = dfs(root->left, rst);
+        int right = dfs(root->right, rst);
+        int seq = max({left+middle, right+middle, middle});
+        int tmp = max({seq, left+right+middle});
         rst = max(tmp, rst);
-        
         return seq;
     }
     int maxPathSum(TreeNode* root) {
         int rst = -INT_MAX;
-        treesum(root, rst);
+        dfs(root, rst);
         return rst;
     }
 };
