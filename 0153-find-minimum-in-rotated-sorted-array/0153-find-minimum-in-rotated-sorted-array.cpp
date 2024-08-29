a0540a46409302ec9ba57227c359a2839e83296d
shangrex <hshangrex@gmail.com>
2024-08-29 10:07:06 -0500

    Time: 3 ms (56.59%), Space: 13 MB (42.31%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
@@ -1,35 +1,24 @@
 class Solution {
-// thought using recursive to solve binary search
-/*
-    int find_min(vector<int>nums, int i, int j){
-        int mid = nums[(i+j)/2];
-        cout << mid << endl;
-        if((i+j)/2-i<1)return min(nums[i], nums[j]);
-        if(nums[i] < nums[j]) return nums[i];
-        else if(nums[i] < mid && mid > nums[j])return find_min(nums, (i+j)/2+1, j);
-        else return find_min(nums, i, (i+j)/2);
-    }
-    int findMin(vector<int>& nums) {
-        if(nums.size()==1)return nums[0];
-        return find_min(nums, 0, nums.size()-1);
-    }
-*/
-// hint -> use while loop to solve binary search
 public:
+    /*
+    Hint: Find the inflection point (that the turning point of the sorted array)
+    */
     int findMin(vector<int>& nums) {
-        int left = 0, right = nums.size()-1, mid;
+        int left = 0, right = nums.size()-1;
         while(left < right){
-            mid = (left+right)/2;
-            // target in right
+            int mid = (left+right) /2;
+            
+            if(nums[left] < nums[right]){
+                return nums[left];
+            }
+            
             if(nums[mid] > nums[right]){
+                // left is ascending
                 left = mid+1;
             }
-            // target in left
             else if(nums[mid] < nums[right]){
+                // right is ascending
                 right = mid;
-            }
-            else {
-                break;
             }
         }
         return nums[left];
