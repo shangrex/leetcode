5b8ca123892acf613c6ec5247635db195648f753
shangrex <hshangrex@gmail.com>
2024-08-28 19:24:51 -0500

    Time: 27 ms (57.08%), Space: 30.2 MB (20.32%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -2,20 +2,18 @@ class Solution {
 public:
     int search(vector<int>& nums, int target) {
         int left = 0, right = nums.size();
-        
         while(left < right){
-            int middle = (left+right)/2;
-            if(nums[middle] == target){
-                return middle;
+            int mid = (left+right)/2;
+            if(nums[mid] == target){
+                return mid;
             }
-            else if(nums[middle] <= target){
-                left = middle+1;
+            else if(nums[mid] > target){
+                right = mid;
             }
-            else {
-                right = middle;
+            else{
+                left = mid+1;
             }
         }
-        
         return -1;
     }
 };
