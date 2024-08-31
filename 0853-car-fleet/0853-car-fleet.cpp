d9331589d92ff99e4d39de4d301b02e343fa17c1
shangrex <hshangrex@gmail.com>
2024-08-31 15:41:53 -0500

    Time: 130 ms (60.46%), Space: 89.8 MB (22.97%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.cpp
+++ 0853-car-fleet/0853-car-fleet.cpp
@@ -4,6 +4,9 @@ public:
     Hint: calculate the remaining time that arrive targe.
     Sorted from the starting position, if only one fleet, then the remaining time should be strict
     decreasing
+    
+    Approach 1. scan from the back
+    Approach 2. monothonic stack 
     */
     int carFleet(int target, vector<int>& position, vector<int>& speed) {
         vector<pair<int, double>>pr;
@@ -13,17 +16,28 @@ public:
         sort(pr.begin(), pr.end(), [](auto &a, auto &b){
             return a.first < b.first;
         });
-        // for(auto i : pr){
-        //     cout << i.first << " " << i.second << endl;
+        
+        // approach 1.
+        // int rst = 1;
+        // double tmp = pr[pr.size()-1].second;
+        // for(int i = pr.size()-2; i >= 0; i--){
+        //     if(pr[i].second > tmp){
+        //         rst++;
+        //         tmp =  pr[i].second;
+        //     }
         // }
-        int rst = 1;
-        double tmp = pr[pr.size()-1].second;
-        for(int i = pr.size()-2; i >= 0; i--){
-            if(pr[i].second > tmp){
-                rst++;
-                tmp =  pr[i].second;
+        // return rst;
+        
+        
+        // approach 2.
+        stack<double>st;
+        for(int i = 0; i < pr.size(); i++){
+            while(!st.empty() && st.top() <= pr[i].second){
+                st.pop();
             }
+            st.push(pr[i].second);
         }
-        return rst;
+        return st.size();
+        
     }
 };
