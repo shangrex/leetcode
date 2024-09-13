4c9616de039b1507091d23bb895c9bdc967b6ffa
shangrex <hshangrex@gmail.com>
2024-09-13 16:32:32 -0500

    Time: 12 ms (64.37%), Space: 26.7 MB (85.31%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -19,30 +19,31 @@ public:
     preorder feature: vector[0] will always be the root
     inorder feature: vector[index], all the left tree is in vector[0:index], and the right tree is in vector[index, vector.size()-1]
     
+    root_index pass by reference
     
     */
-    
-    TreeNode* check(vector<int> &preorder, vector<int>&inorder, int &root_index, int left_bound, int right_bound){
-        if(left_bound > right_bound)return NULL;
-        
-        TreeNode *root = new TreeNode(preorder[root_index]);
-        
-        int pivot=left_bound;
-        for(int i = left_bound; i <= right_bound; i++){
-            if(preorder[root_index] == inorder[i]){
-                pivot = i;
+    TreeNode *build(vector<int>&preorder, vector<int>&inorder, int& idx, int left, int right){
+        if(left > right) return NULL;
+        if(idx >= preorder.size()) return NULL;
+        cout << idx << " " << left << " " << right << endl;
+        TreeNode *root = new TreeNode(preorder[idx]);
+        int split_point = -1;
+        for(int i = left; i <= right; i++){
+            if(inorder[i] == preorder[idx]){
+                split_point = i;
                 break;
             }
         }
-        // root_index should pass by reference, since every root node would only calculate once.
-        root_index++;
-        // pass inorder left_bound and right bound
-        root->left = check(preorder, inorder, root_index , left_bound, pivot-1);
-        root->right = check(preorder, inorder, root_index, pivot+1, right_bound);
+        idx++;
+        root->left = build(preorder, inorder, idx, left, split_point-1);
+        root->right = build(preorder, inorder, idx, split_point+1, right);
         return root;
     }
+    
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        int root_index=0;
-        return check(preorder, inorder, root_index, 0, preorder.size()-1);
+        TreeNode* head;
+        int idx = 0;
+        head = build(preorder, inorder, idx, 0, inorder.size()-1);
+        return head;
     }
 };
