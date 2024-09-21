08d8412620773528dafaa8b6d7e79c9453562773
shangrex <hshangrex@gmail.com>
2024-09-21 18:49:47 -0500

    Time: 500 ms (82.25%), Space: 555.3 MB (57.70%) - LeetHub

--- 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.cpp
+++ 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.cpp
@@ -1,52 +1,60 @@
-class Trie {
+class TrieNode {
     public:
-        bool is_end;
-        map<char, Trie*>next;
-        Trie() {
-            this->is_end = false;
+    bool is_end;
+    TrieNode *node[26];
+    TrieNode(){
+        is_end = false;
+        for(int i = 0; i < 26; i++){
+            node[i] = NULL;
         }
-        
+    }
 };
+
 class WordDictionary {
 public:
-    Trie *root;
+    TrieNode *root;
     WordDictionary() {
-        root = new Trie();
+       root = new TrieNode();
     }
     
     void addWord(string word) {
-        Trie *cur = root;
+        TrieNode *cur = root;
         for(int i = 0; i < word.length(); i++){
-            if(cur->next.count(word[i]) == 0){
-                Trie *tmp = new Trie();
-                cur->next[word[i]] = tmp;
-                cur->next['.'] = tmp;
-            }
-            cur = cur->next[word[i]];
+            //cout << word[i];
+            if(cur->node[word[i]-'a'] == NULL) cur->node[word[i]-'a'] = new TrieNode();
+            cur = cur->node[word[i]-'a'];
         }
         cur->is_end = true;
     }
     
-    bool search(string word) {
-        return helper(root, word, 0);
+    bool helper(string &word, int idx, TrieNode *cur){
+        bool flag = false;
+        
+        if(idx == word.length()) {
+            return  cur->is_end;
+        }
+        
+        if(word[idx] != '.'){
+            if(cur->node[word[idx]-'a'] == NULL) return false;
+            cur = cur->node[word[idx]-'a'];
+            return helper(word, idx+1, cur);
+        }
+        else {
+            TrieNode *tmp;
+            for(int i = 0; i < 26; i++){
+                if(cur->node[i] != NULL) {
+                    tmp = cur->node[i];
+                    flag |= helper(word, idx+1, tmp);
+                }
+            }
+        }
+        
+        return flag;
     }
     
-    // when encounter '.', then test different way to go down the trie
-    bool helper(Trie* cur, string word, int k){
-        if(word.length() == k)return cur->is_end;
-        bool check = false;
-        if(word[k] == '.'){
-            for(auto j : cur->next){
-                check = helper(j.second, word, k+1);
-                if(check)return true;
-            }
-            return false;
-        }
-        else if(cur->next.count(word[k]) != 0){
-            cur = cur->next[word[k]];
-            return helper(cur, word, k+1);
-        }
-        else return false;
+    bool search(string word) {
+        TrieNode *cur = root;
+        return helper(word, 0, cur);
     }
 };
 
