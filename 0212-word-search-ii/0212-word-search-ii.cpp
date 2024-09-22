d1abef0ba07562c771feec2902072255b6acba97
shangrex <hshangrex@gmail.com>
2024-09-21 23:42:40 -0500

    Time: 411 ms (72.59%), Space: 18.8 MB (73.84%) - LeetHub

--- 0212-word-search-ii/0212-word-search-ii.cpp
+++ 0212-word-search-ii/0212-word-search-ii.cpp
@@ -1,69 +1,93 @@
-class Trie {
+class TrieNode {
     public:
-        bool is_end = false;
-        Trie* next[26];
-        string word = "";
-        Trie(){
-            for(int i = 0; i < 26; i++){
-                next[i] = NULL;
-            }
+        bool is_end;
+        unordered_map<char, TrieNode*> next;
+        string word;
+        TrieNode() {
+            is_end = false;
+            word = "";
         }
 };
 
-void dfs(vector<vector<char>>&board, Trie* cur, vector<string>& rst, int i, int j){
-
-    if(i < 0 || i >= board.size() || j < 0 || j >= board[0].size())return;
-    if(board[i][j] == '#')return;
-
-    if(cur->next[board[i][j]-'a'] != NULL){
-        int node = board[i][j]-'a';
-        cur = cur->next[node];
-        
-        if(cur->is_end){
-            rst.push_back(cur->word); 
-            cur->is_end = false;    
-        }
-        
-        char tmp = board[i][j];  
-        board[i][j] = '#';
-        // exist
-        dfs(board, cur, rst, i+1, j);
-        dfs(board, cur, rst, i-1, j);
-        dfs(board, cur, rst, i, j+1);
-        dfs(board, cur, rst, i, j-1);
-        
-        board[i][j] = tmp;
-    }
-}
-
 class Solution {
 public:
+    /*
+    Construct Trie in words to reduce replicate search
+    
+    */
+    
+    void dfs(vector<vector<char>> &board, TrieNode* cur, int h, int w, vector<string> &rst){
+        int n = board.size(), m = board[0].size();
+        if(h >= n || w >= m || h < 0 || w < 0){
+            return;
+        }
+       // cout << h << " " << w  << board[h][w] << endl;            
+        
+        if(cur->is_end){
+            rst.push_back(cur->word);
+            cur->is_end = false;
+            //return;
+        }
+        
+        char store_char = board[h][w];
+        TrieNode *store_cur = cur;
+        board[h][w] = '#';
+        //cout << board[h][w];
+        if(h+1 < n){
+            if(cur->next.find(board[h+1][w]) != cur->next.end()){
+                store_cur = cur->next[board[h+1][w]];
+                dfs(board, store_cur, h+1, w, rst);
+            }
+        }
+        if(w+1 < m){
+            if(cur->next.find(board[h][w+1]) != cur->next.end()){
+                store_cur = cur->next[board[h][w+1]];
+                dfs(board, store_cur, h, w+1, rst);
+            }
+        }
+        if(h-1 >= 0){
+            if(cur->next.find(board[h-1][w]) != cur->next.end()){
+                store_cur = cur->next[board[h-1][w]];
+                dfs(board, store_cur, h-1, w, rst);
+            }
+        }
+        if(w-1 >= 0){
+            if(cur->next.find(board[h][w-1]) != cur->next.end()){
+                store_cur = cur->next[board[h][w-1]];
+                dfs(board, store_cur, h, w-1, rst);
+            }
+        }
+        
+        board[h][w] = store_char;
+    }
+    
     vector<string> findWords(vector<vector<char>>& board, vector<string>& words) {
         
-        // construct Trie
-        Trie* root = new Trie();
-        for(int i = 0; i < words.size(); i++){
-            Trie* cur = root;
-            for(int j = 0; j < words[i].length(); j++){
-                Trie* tmp = new Trie();
-                int node = words[i][j]-'a';
-                if(cur->next[node] == NULL) cur->next[node] = tmp;
-                cur = cur->next[node];
+        TrieNode *root = new TrieNode();
+        TrieNode *cur;
+        // constrcut trie
+        for(auto &word : words){
+            cur = root;
+            for(int i = 0; i < word.length(); i++){
+                if(cur->next.find(word[i]) == cur->next.end()) {
+                    cur->next[word[i]] = new TrieNode();
+                }
+                cur = cur->next[word[i]];
             }
             cur->is_end = true;
-            cur->word = words[i];
+            cur->word = word;
         }
         
         vector<string>rst;
         
-        for(int i = 0; i < board.size(); i++){
+        cur = root;
+        // triverse in the graph using dfs
+        for(int i = 0 ; i < board.size(); i++){
             for(int j = 0; j < board[0].size(); j++){
-                Trie* cur = root;
-                int node = board[i][j]-'a';
-                if(cur->next[node] != NULL){
-                    // exist
-                    dfs(board, cur, rst, i, j);
-                }
+                if(cur->next.find(board[i][j]) != cur->next.end()){
+                    //cout << board[i][j] << endl;
+                    dfs(board,  cur->next[board[i][j]], i, j, rst);                    
+                } 
             }
         }
         
