bf950a2ae5a31ebe419b3f0138adaac557d05c9f
shangrex <hshangrex@gmail.com>
2024-10-14 11:56:46 -0500

    Time: 84 ms (5.53%), Space: 16.4 MB (75.19%) - LeetHub

--- 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.cpp
+++ 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.cpp
@@ -1,37 +1,32 @@
 class Solution {
 public:
-    /*
-    method 1 use union find
     
-    */
     unordered_map<int, int>mp;
+    int rst = 0;
+    int Find(int a){
+        if(mp[a] == a) return a;
+        else return Find(mp[a]);
+    }
+    
+    void Union(int a, int b){
+        int root_a = Find(a);
+        int root_b = Find(b);
+        if(root_a != root_b) {cout << root_a << " " << root_b << endl;this->rst--;mp[root_b] = root_a;}
+        
+    }
     
     int countComponents(int n, vector<vector<int>>& edges) {
-        // initialize
-        for(int i = 0;i < n; i++){
+        for(int i = 0; i < n; i++){
             mp[i] = i;
         }
-        
-        for(int i = 0;i < edges.size(); i++){
-            bool check = Union(edges[i][0], edges[i][1]);
-            if(check)n--;
+        this->rst = n;
+        for(auto &edge : edges){
+            Union(edge[0], edge[1]);
         }
-        return n;
-    }
-    
-    bool Union(int a, int b){
-        int head_a = Find(a);
-        int head_b = Find(b);
-        if(head_a != head_b){
-            mp[head_b] = head_a;
-            return true;
-        }
-        else return false;
         
-    }
-    
-    int Find(int value){
-        if(mp[value] == value)return value;
-        else return Find(mp[value]);
+        // for(int i = 0; i < n; i++){
+        //     if(mp[i] == i){cout << i << endl;this->rst++;}
+        // }
+        return this->rst;
     }
 };
