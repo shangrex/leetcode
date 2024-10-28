ebb94ed5b61b5c33c7a06075f52340f974c40cbc
shangrex <hshangrex@gmail.com>
2024-10-27 23:34:09 -0500

    Time: 135 ms (34.20%), Space: 26.8 MB (37.93%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,3 +1,14 @@
+    /*
+    approach 1: two pointer
+        should consider two case, the odd length and even length (or use other ways to check the same continuous charactrers)
+        x2 x1 a y1 y2 -> compare x1 ,y1 if it is the same then compare x2 ,y2 and so on
+    approach 2: DP
+        dp[start, end] inclusive means the boolen valuse of palindromic
+        start+1 = end, start == end is true 
+        start+2 = end, start == end && start+1, end-1 is true
+        start+3 = end, start
+    
+    */
 class Solution {
 public:
     /*  baab
@@ -5,6 +16,9 @@ public:
               dp[i-1][i] = true if s[i-1] == s[i]
     dp[i-1][i+1]  then s[i-1] == s[i+1]
     */
+    
+    
+    
     string longestPalindrome(string s) {
         int n = s.length();
         vector<vector<bool>> dp(n, vector<bool>(n, false));
