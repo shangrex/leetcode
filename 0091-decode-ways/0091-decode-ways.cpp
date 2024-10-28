a4ed012a9b430736aefda76d7ab3c421b16668e4
shangrex <hshangrex@gmail.com>
2024-10-28 14:45:44 -0500

    Time: 0 ms (100.00%), Space: 8.2 MB (50.93%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -1,25 +1,22 @@
 class Solution {
 public:
     int numDecodings(string s) {
-        // p(n-1)+p'(n-2)
-        int rst = 0;
-        vector<int> s_tmp(s.length()+1, 0), dp(s.length()+1, 0);
-        dp[s.length()]=1;
-        // transform string to int number in vector
-        for(int i = 0; i < s.length(); i++){
-            s_tmp[i] = int(s[i])-'0';
+        int n = s.length();
+        vector<int>dp(n+1, 0) ,s2(n, 0);
+        
+        for(int i = 0; i < n; i++){
+            s2[i] = s[i] - '0';
         }
-        for(int i = s.length()-1; i >= 0; i--){
-            if(s_tmp[i] != 0){
-                dp[i]+=dp[i+1];
+        dp[n] = 1;
+        if(s2[n-1] != 0)dp[n-1] = 1;
+        for(int i = n-2; i >= 0; i--){
+            if(s2[i] != 0){
+                dp[i] += dp[i+1]; 
             }
-            if(i < s.length()-1){
-                if((s_tmp[i] == 1) || (s_tmp[i] == 2 && s_tmp[i+1] < 7) ){
-                    dp[i] += dp[i+2];
-                }
+            if((s2[i] == 1  && s2[i+1] >= 0) || (s2[i] == 2 && s2[i+1] < 7)){
+                dp[i] += dp[i+2];
             }
         }
-        // cout << dp[0] << endl;
         return dp[0];
     }
 };
