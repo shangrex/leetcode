d889c8987bc2f6f15ac0ebe5fc17e25eeea40d50
shangrex <hshangrex@gmail.com>
2024-10-27 23:33:15 -0500

    Time: 127 ms (37.23%), Space: 26.8 MB (33.70%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,51 +1,37 @@
 class Solution {
 public:
-    /*
-    approach 1: two pointer
-        should consider two case, the odd length and even length (or use other ways to check the same continuous charactrers)
-        x2 x1 a y1 y2 -> compare x1 ,y1 if it is the same then compare x2 ,y2 and so on
-    approach 2: DP
-        dp[start, end] inclusive means the boolen valuse of palindromic
-        start+1 = end, start == end is true 
-        start+2 = end, start == end && start+1, end-1 is true
-        start+3 = end, start
-    
+    /*  baab
+    palidrome dp[i][i] =. true
+              dp[i-1][i] = true if s[i-1] == s[i]
+    dp[i-1][i+1]  then s[i-1] == s[i+1]
     */
     string longestPalindrome(string s) {
-        string rst;
         int n = s.length();
-        vector<vector<bool>>dp(n, vector<bool>(n));
-        
-        // initialize
+        vector<vector<bool>> dp(n, vector<bool>(n, false));
+            
         for(int i = 0; i < n; i++){
             dp[i][i] = true;
         }
-        // state transition
-        for(int i = 1; i < n; i++){
-            if(s[i] == s[i-1])dp[i-1][i] = true;
+        
+        
+        int left = 0, max_length = 1;
+        for(int i = 0; i < n; i++){
+            if(i > 0 && s[i-1] == s[i]) {
+                dp[i-1][i] = true;
+                if(max_length < 3) {left = i-1, max_length = 2;}
+            }
+            
             for(int j = i-2; j >= 0; j--){
-                if(s[i] == s[j] && dp[j+1][i-1] == true)dp[j][i] = true;
-                else dp[j][i] = false;
+                if(s[j] == s[i] && dp[j+1][i-1]){
+                    dp[j][i] = true;
+                    if(max_length < i-j+1) {left = j; max_length = i-j+1;}
+                }
             }
         }
         
-    
-        // find the longest palindromic substring
-        int rst_start = 0, rst_length = 0, tmp_start = 0, tmp_length = 0;
-        for(int i = 0; i < n; i++){
-            for(int j = i; j < n; j++){
-                if(dp[i][j] == true && tmp_length < j-i+1){
-                    tmp_start = i;
-                    tmp_length = j-i+1;
-                }
-            }
-            if(tmp_length > rst_length){
-                rst_length = tmp_length;
-                rst_start = tmp_start;
-            }
-            tmp_length = 0;
-        }
-        rst = s.substr(rst_start, rst_length);
-        return rst;
+        return s.substr(left, max_length);
+        
+        
+        
     }
 };
