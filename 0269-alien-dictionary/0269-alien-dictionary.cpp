101cb6dc13523f4bc93ee1761d5566012a36415c
shangrex <hshangrex@gmail.com>
2024-10-27 22:42:57 -0500

    Time: 7 ms (9.68%), Space: 12.8 MB (45.59%) - LeetHub

--- 0269-alien-dictionary/0269-alien-dictionary.cpp
+++ 0269-alien-dictionary/0269-alien-dictionary.cpp
@@ -1,67 +1,66 @@
 class Solution {
 public:
-    /*
+        /*
     topology sort to check the lexicographical order and check if there is cycle
     
     be careful about the definiton of lexicographical order.
     we are checking character by character to see where the mismatch happens first and using that to create edges.
     and notice there is a failed input, the prefix should be first, but the test case have "abc" "ab", so in this failed case, we should out put "" not cab
     
+    topology sort
+    1. construct graph and indegree
+    2. push indegree 0 to queue
+    3. bfs and if indegree == 1 then add it to queue, then indegree--
     */
     string alienOrder(vector<string>& words) {
-        if(words.size() == 0)return "";
+        unordered_map<char, set<char> >graph;
         unordered_map<char, int>indegree;
-        unordered_map<char, unordered_set<char>>graph;
         
-        // initialize
         for(int i = 0; i < words.size(); i++){
             for(int j = 0; j < words[i].length(); j++){
                 indegree[words[i][j]] = 0;
             }
         }
         
-        // build graph
-        for(int i = 0; i < words.size() - 1; i++){
+        for(int i = 0 ; i < words.size()-1; i++){
             string cur = words[i];
             string next = words[i+1];
-            int tmp = min(cur.length(), next.length());
-            for(int j = 0; j < tmp; j++){
+            if(cur.find(next) == 0 && cur.length() > next.length()){
+                return "";
+            }
+            for(int j = 0; j < min(cur.length(), next.length()); j++){
                 if(cur[j] != next[j]){
-                    if(graph[cur[j]].count(next[j]) == 0){
-                        indegree[next[j]]++;
+                   if(graph[cur[j]].count(next[j]) == 0){
                         graph[cur[j]].insert(next[j]);
+                        indegree[next[j]] += 1;
                     }
+                    
                     break;
                 }
             }
-            if(cur.find(next) == 0 && cur.length() > next.length()){
-                cout << "invalid input" << endl;
-                return "";
-            }
         }
-        // topology sort
-        queue<int>q;
-        string ans = "";
         
-        for(auto i : indegree){
+        
+        queue<char> q;
+        for(auto & i : indegree){
             if(i.second == 0){
+                cout << i.first << endl;
                 q.push(i.first);
             }
         }
         
+        string ans = "";
         while(!q.empty()){
-            char c = q.front();
-            ans += c;
+            char t = q.front();
+            ans += t;
             q.pop();
-            for(auto &i : graph[c]){
-                indegree[i]--;
-                if(indegree[i] == 0){
-                    q.push(i);
+            for(auto &s : graph[t]){
+                cout << s << indegree[s] << endl;
+               if(--indegree[s] == 0){
+                    q.push(s);
                 }
-            }                
-            
+            }
         }
-        
-        return ans.length() == indegree.size() ? ans : "";
+        return ans.size() == indegree.size() ? ans : "";
     }
 };
