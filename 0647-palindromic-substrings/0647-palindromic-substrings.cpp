b810c16b6141eef00538ca33b7b1646c4472c28b
shangrex <hshangrex@gmail.com>
2024-10-27 23:23:06 -0500

    Time: 27 ms (43.00%), Space: 11.5 MB (59.66%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,27 +1,23 @@
 class Solution {
 public:
-    /*
-    See the longest palindromic string
-    */
     int countSubstrings(string s) {
-        int n = s.length(), rst = 0;
-        vector<vector<bool>> dp(n, vector<bool>(n));
+        int n = s.length();
+        vector<vector<bool>> dp(n, vector<bool>(n, false));
         
+        
+        int rst = n;
         for(int i = 0; i < n; i++){
-            dp[i][i] = true;
-            rst++;
+                dp[i][i] = true;
         }
         
-        for(int i = 1; i < n; i++){
-            // special case aa
-            if(s[i] == s[i-1]){dp[i-1][i] = true;rst++;}
+        for(int i = 0; i < n; i++){
+            if(i > 0 && s[i-1] == s[i]){dp[i-1][i] = true;rst++;}
             
             for(int j = i-2; j >= 0; j--){
-                if(s[i] == s[j] && dp[j+1][i-1] == true){
+                if(s[j] == s[i] && dp[j+1][i-1]){
                     dp[j][i] = true;
                     rst++;
                 }
-                else dp[j][i] = false;
             }
         }
         return rst;
