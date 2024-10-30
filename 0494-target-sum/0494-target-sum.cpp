fe833fd5a01c6ee1e43b6b87890e55cecbf7c691
shangrex <hshangrex@gmail.com>
2024-10-30 13:42:30 -0500

    Time: 3662 ms (5.05%), Space: 385.3 MB (5.53%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,6 +1,4 @@
-class Solution {
-public:
-    
+/* backtrack
     void backtrack(vector<int>&nums, int idx, int sum, vector<int>&ans, int& target ){
         if(idx >= nums.size()){
             // cout << sum << endl;
@@ -22,5 +20,32 @@ public:
             }
         }
         return rst;
+    }
+*/
+class Solution {
+public:
+    /*
+    Approach 1.backtrack
+    Appraoch 2.memo 
+    */
+    int rst = 0;
+    int backtrack(vector<int>&nums, int idx, int sum, int& target, vector<vector<int>> &memo){
+        if(idx >= nums.size()){
+            // cout << sum << endl;
+            if(target == sum)return 1;
+            return 0;
+        }
+        //cout << idx<< " " << sum+nums[idx] << endl;
+        
+        if(memo[idx][sum+5000] != INT_MIN) return memo[idx][sum+5000];
+        int a = backtrack(nums, idx+1, sum+nums[idx], target, memo);
+        int b = backtrack(nums, idx+1, sum-nums[idx], target, memo);
+        memo[idx][sum+5000] = a+b;
+        return memo[idx][sum+5000];
+    }
+    int findTargetSumWays(vector<int>& nums, int target) {
+        vector<int> ans;
+        vector<vector<int>> memo(nums.size(), vector<int>(1010000, INT_MIN));
+        return backtrack(nums, 0, 0, target, memo);
     }
 };
