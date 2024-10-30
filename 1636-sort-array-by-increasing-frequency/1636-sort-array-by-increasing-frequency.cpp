2972e6f3aaed28d728ad25dc64bc65ade4fccda8
shangrex <hshangrex@gmail.com>
2024-10-29 19:21:42 -0500

    Time: 7 ms (20.23%), Space: 15.1 MB (59.85%) - LeetHub

---
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> frequencySort(vector<int>& nums) {
+        unordered_map<int, int>mp;
+        for(auto &num : nums){
+            mp[num] += 1;
+        }
+        
+        sort(nums.begin(), nums.end(), [&](int a, int b){
+            if(mp[a] < mp[b]){
+                // cout << mp[b] << b << endl;
+                // cout << mp[a] <<a <<  endl;
+                return true;
+            }
+            else if(mp[a] > mp[b]){
+                // cout << mp[b] << b << endl;
+                // cout << mp[a] <<a <<  endl;
+                return false;
+            }
+            else {
+                if(a > b)return true;
+                else return false;
+            }
+        });
+        return nums;
+    
+    }
+};
