b856969803009be2f759f3ca9752bfba4dfdc03a
shangrex <hshangrex@gmail.com>
2024-10-31 12:23:15 -0500

    Time: 295 ms (5.08%), Space: 21.2 MB (5.79%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,5 +1,9 @@
 class Solution {
 public:
+    /*
+    Appraoch 1. bottom up DP
+    dp[i] store the step from 0
+    transition: dp[i+j] = min(dp[i+j], 1+dp[i])
     int jump(vector<int>& nums) {
         int boarder = 0;
         int rst = 0;
@@ -16,5 +20,27 @@ public:
             }
         }
         return dp[n-1];
+    }
+
+    Appraoch 2. top down DP
+    transition : dp[pos] = min(dp[pos], )
+    */
+
+    int solve(vector<int>&nums, vector<int>&dp, int pos){
+        if(pos >= nums.size()-1) return 0;
+
+        if(dp[pos] != 101010) return dp[pos];
+
+        for(int i = 1; i <= nums[pos]; i++){
+            if(pos+i < nums.size())
+                dp[pos] = min(dp[pos], 1+solve(nums, dp, pos+i));
+        }
+        return dp[pos];
+    }
+    int jump(vector<int>& nums) {
+        int n = nums.size();
+        vector<int>dp(n, 101010);
+        return solve(nums, dp, 0);
+        
     }
 };
