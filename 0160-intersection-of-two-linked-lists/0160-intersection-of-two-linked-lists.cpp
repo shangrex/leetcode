7c9cde605b22de17ff8ba71b1f173ea0d4e1b322
shangrex <hshangrex@gmail.com>
2024-11-06 14:00:07 -0600

    Time: 31 ms (89.84%), Space: 17.4 MB (64.63%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -7,7 +7,12 @@
  * };
  */
 class Solution {
-public:
+    /*
+        Hint :the intersection point to the listend is the same in both A and B
+        -> find the first same point
+        
+        Approach 1. set like hash table
+
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
         unordered_set<ListNode*>s;
         ListNode*cur = headA;
@@ -24,5 +29,49 @@ public:
             cur = cur->next;
         }
         return NULL;
+    }
+        Approach 2. use hint and check the first same point
+    */
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        int la = 0;
+        ListNode *cur = headA;
+        while(cur){
+            la++;
+            cur = cur->next;
+        }
+        int lb = 0;
+        cur = headB;
+        while(cur){
+            lb++;
+            cur = cur->next;
+        }
+
+        if(la > lb){
+            int diff = la-lb;
+            cur = headA;
+            for(int i = 0; i < diff; i++) cur = cur->next;
+            ListNode *t = headB;
+            while(t != cur){
+                if(!t)break;
+                t = t->next;
+                cur = cur->next;
+            }
+            return cur;
+        }
+        else {
+            int diff = lb -la;
+            cur = headB;
+            //cout << headB->val<<endl;
+            for(int i = 0; i < diff; i++) cur = cur->next;
+            ListNode *t = headA;
+            while(t != cur){
+                if(!t)break;
+                cout << t->val << " " << cur->next;
+                t = t->next;
+                cur = cur->next;
+            }
+            return cur;
+        }
     }
 };
