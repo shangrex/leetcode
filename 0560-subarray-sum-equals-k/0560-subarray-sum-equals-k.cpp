9b225af695eef9aeada82efcc348854803683d53
shangrex <hshangrex@gmail.com>
2024-11-06 14:22:31 -0600

    Time: 1404 ms (21.91%), Space: 38.9 MB (75.81%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,27 +1,19 @@
 class Solution {
 public:
-    /*
-    approach 1: cumulative sum
-    time: O(N^2)
-    space: O(N)
-    sum[i] = nums[0]+...+nums[i]
-    sum[end] - sum[start] = k
-    
-    approach 2: 
-    */
     int subarraySum(vector<int>& nums, int k) {
-        int ans = 0, sum = 0;
-        // the frequency of cumulative sum
-        unordered_map<int, int>mp;
-        mp[0] = 1;
-        for(int i = 0;i < nums.size(); i++){
-            sum += nums[i];
-            if(mp.find(sum-k) != mp.end()){
-                ans += mp[sum-k];
-            }
-            if(mp.find(sum) != mp.end())mp[sum] += 1;
-            else mp[sum] = 1;
+        vector<long long> prefix;
+        long long cum = 0;
+        for(int i = 0; i < nums.size(); i++){
+            cum += nums[i];
+            prefix.push_back(cum);
         }
-        return ans;
+        int rst = 0;
+        for(int i = 0; i < nums.size(); i++){
+            if(prefix[i] == k) rst++;
+            for(int j = 0; j < i; j++){
+                if(prefix[i] - prefix[j] == k)rst++;
+            }
+        }
+        return rst;
     }
 };
