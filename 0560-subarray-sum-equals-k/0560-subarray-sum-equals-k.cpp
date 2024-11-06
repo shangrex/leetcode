cd6a413bffa59279c6cf41c9b811e16fe57eeac6
shangrex <hshangrex@gmail.com>
2024-11-06 14:24:41 -0600

    Time: 1403 ms (21.91%), Space: 38.8 MB (75.81%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,5 +1,22 @@
 class Solution {
 public:
+/*
+    int subarraySum(vector<int>& nums, int k) {
+        int ans = 0, sum = 0;
+        // the frequency of cumulative sum
+        unordered_map<int, int>mp;
+        mp[0] = 1;
+        for(int i = 0;i < nums.size(); i++){
+            sum += nums[i];
+            if(mp.find(sum-k) != mp.end()){
+                ans += mp[sum-k];
+            }
+            if(mp.find(sum) != mp.end())mp[sum] += 1;
+            else mp[sum] = 1;
+        }
+        return ans;
+    }
+*/
     int subarraySum(vector<int>& nums, int k) {
         vector<long long> prefix;
         long long cum = 0;
