c3c1cd5c971b4895055621a88daf4d59d3131038
shangrex <hshangrex@gmail.com>
2024-11-13 19:41:01 -0600

    Time: 2 ms (18.58%), Space: 16.7 MB (94.85%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -2,26 +2,26 @@ class Solution {
 public:
     int m, n;
 
-    void dfs(vector<vector<int>> &image, int sr, int sc, int org_color, int color, vector<vector<bool>>&visit){
+    void dfs(vector<vector<int>> &image, int sr, int sc, int org_color, int color){
         int m = image.size();
         int n = image[0].size();
-        if(sr < 0 || sc < 0 || sr >= m || sc >= n) { return ;}
-        visit[sr][sc] = true;
+        cout << m << n;
+
         if(image[sr][sc] == org_color) {
             image[sr][sc] = color;
+            if(sr+1 < m && image[sr+1][sc] == org_color)        
+                dfs(image, sr+1, sc, org_color, color);
+            if(sr-1 >= 0 && image[sr-1][sc] == org_color)        
+                dfs(image, sr-1, sc, org_color, color);
+            if(sc+1 < n && image[sr][sc+1] == org_color)        
+                dfs(image, sr, sc+1, org_color, color);
+            if(sc-1 >= 0 && image[sr][sc-1] == org_color)        
+                dfs(image, sr, sc-1, org_color, color);
         }
-        if(sr+1 < m && image[sr+1][sc] == org_color && visit[sr+1][sc] == false)        
-            dfs(image, sr+1, sc, org_color, color, visit);
-        if(sr-1 >= 0 && image[sr-1][sc] == org_color && visit[sr-1][sc] == false)        
-            dfs(image, sr-1, sc, org_color, color, visit);
-        if(sc+1 < n && image[sr][sc+1] == org_color && visit[sr][sc+1] == false)        
-            dfs(image, sr, sc+1, org_color, color, visit);
-        if(sc-1 >= 0 && image[sr][sc-1] == org_color && visit[sr][sc-1] == false)        
-            dfs(image, sr, sc-1, org_color, color, visit);
     }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        vector<vector<bool>> visit(image.size(), vector<bool>(image[0].size(), false));
-        dfs(image, sr, sc, image[sr][sc], color, visit);
+        if(color != image[sr][sc])
+            dfs(image, sr, sc, image[sr][sc], color);
         return image;
     }
 };
