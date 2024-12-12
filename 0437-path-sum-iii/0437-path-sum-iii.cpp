846d9992fa19a2f8b8c584ecce5c1a726c3f8369
shangrex <hshangrex@gmail.com>
2024-12-12 09:56:41 -0600

    Time: 10 ms (54%), Space: 18.9 MB (57.69%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.cpp
+++ 0437-path-sum-iii/0437-path-sum-iii.cpp
@@ -11,38 +11,21 @@
  */
 class Solution {
 public:
-    void helper(TreeNode* root, long long int sum, int &ans, unordered_map<long long int, long long int>mp, int target){
-        if(!root)return;
-        // 10 15 18 21
-        sum += root->val;
-        
-        
-        if(mp.find(sum-target) != mp.end()){
-            if(mp[sum-target] > 0)ans += mp[sum-target];
-        }
-        
-        // add prefix sum 
-        if(mp.find(sum) != mp.end()){
-            mp[sum]+=1;
-        }
-        else {
-            cout << sum << endl;
-            mp[sum] = 1;
-        }
-        
-        // triverse
-        helper(root->left, sum, ans, mp, target);
-        helper(root->right, sum, ans, mp, target);
-        
-        // remove prefix
-        mp[sum] -= 1;
-        
-    }
+    int rst = 0;
     int pathSum(TreeNode* root, int targetSum) {
-        int ans = 0;
-        unordered_map<long long int, long long int>mp; // key: value, value: count
-        mp[0] = 1;
-        helper(root, 0, ans, mp, targetSum);
-        return ans;
+        if(!root)return 0;
+        findpath(root, targetSum);
+        pathSum(root->left, targetSum);
+        pathSum(root->right, targetSum);
+        return rst;
+    }
+
+    void findpath(TreeNode*root, long long targetSum){
+        if(!root) return;
+        if(root->val == targetSum){
+            rst++;
+        }
+        findpath(root->left, targetSum-root->val);
+        findpath(root->right, targetSum-root->val);
     }
 };
