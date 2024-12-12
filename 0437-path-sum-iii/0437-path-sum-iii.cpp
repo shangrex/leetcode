72b831d8afc90bd5fe1e7e501bbc756779271068
shangrex <hshangrex@gmail.com>
2024-12-12 12:27:56 -0600

    Time: 4 ms (85.79%), Space: 20.7 MB (43.65%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.cpp
+++ 0437-path-sum-iii/0437-path-sum-iii.cpp
@@ -9,9 +9,10 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
-public:
-    int rst = 0;
+ /*
+ appraoch 1.
+     int rst = 0;
+    unordered_map<int, int>mp;
     int pathSum(TreeNode* root, int targetSum) {
         if(!root)return 0;
         findpath(root, targetSum);
@@ -27,5 +28,36 @@ public:
         }
         findpath(root->left, targetSum-root->val);
         findpath(root->right, targetSum-root->val);
+    }
+ */
+class Solution {
+public:
+    /*
+    Appraoch 1. DFS, be careful to count replicate. To count the new node, need to PathSum again
+
+    Approach 2. prefix sum
+    */
+    int rst = 0;
+    unordered_map<long long, long long>mp;
+    int pathSum(TreeNode* root, int targetSum) {
+        long long cur_sum = 0;
+        mp[0] = 1;
+        findpath(root, targetSum, cur_sum);
+        return rst;
+    }
+
+    void findpath(TreeNode*root, long long targetSum, long long & cur_sum){
+        if(!root) return;
+
+        cur_sum += root->val;
+        if(mp.find(cur_sum - targetSum) != mp.end()){
+            rst += mp[cur_sum - targetSum];
+        }
+        mp[cur_sum] += 1;
+
+        findpath(root->left, targetSum, cur_sum);
+        findpath(root->right, targetSum, cur_sum);
+        mp[cur_sum] -= 1;
+        cur_sum -= root->val;
     }
 };
