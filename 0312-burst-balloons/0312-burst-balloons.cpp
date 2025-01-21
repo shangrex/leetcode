f0e8c2557535f3ef9a1bb81f02e88a6d333e6358
shangrex <hshangrex@gmail.com>
2025-01-20 22:23:05 -0600

    Time: 422 ms (5.05%), Space: 15.1 MB (6.66%) - LeetHub

---
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    /*
+    Hint: Divid and Conquer
+    */
+
+    int dp(vector<vector<int>> &memo, vector<int>&nums, int left, int right){
+        if(left > right) return 0;
+        int max_coin = 0;
+        if(memo[left][right] != -1) return memo[left][right];
+
+        for(int i = left; i <= right; i++){
+            int coin = nums[left-1]*nums[i]*nums[right+1];
+            coin = coin + dp(memo, nums, left, i-1)+dp(memo, nums, i+1, right);
+            max_coin = max(max_coin, coin);
+        }
+        memo[left][right] = max_coin;
+        return max_coin;
+    }   
+    int maxCoins(vector<int>& nums) {
+        // to avoid edge case
+        nums.insert(nums.begin(), 1);
+        nums.insert(nums.end(), 1);
+        int n = nums.size();
+        vector<vector<int>> memo(n+1, vector<int>(n+1, -1));
+        return dp(memo, nums, 1, nums.size()-2);
+    }
+};
