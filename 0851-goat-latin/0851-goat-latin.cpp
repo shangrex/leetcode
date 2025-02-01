3b4b9b3e6664b0eefd96329443b15734a455769b
shangrex <hshangrex@gmail.com>
2025-02-01 15:40:23 -0600

    Time: 3 ms (15%), Space: 9 MB (77.37%) - LeetHub

---
+++ 0851-goat-latin/0851-goat-latin.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    string toGoatLatin(string sentence) {
+        string rst = "";
+        string ap_a = "";
+        bool flag = true;
+        while(flag){
+            int sp = sentence.find(' ');
+            cout << sp << endl;
+            if(sp < 0)flag = false;
+            string sp_str = sentence.substr(0, sp);
+            sentence = sentence.substr(sp+1);
+            ap_a.append("a");
+            if(sp_str[0] == 'a' || sp_str[0] == 'e' || sp_str[0] == 'i' ||
+                sp_str[0] == 'o' || sp_str[0] == 'u' || sp_str[0] == 'A' || 
+                sp_str[0] == 'E' || sp_str[0] == 'I' ||
+                sp_str[0] == 'O' || sp_str[0] == 'U'){
+                    sp_str.append("ma");
+                }
+            else {
+                // remove first letter
+                string ap_str = sp_str.substr(0, 1);
+                sp_str = sp_str.substr(1).append(ap_str+"ma");
+            }
+
+            sp_str.append(ap_a);
+            if(flag)rst.append(sp_str+" ");
+            else rst.append(sp_str);
+        }        
+        return rst;
+    }
+};
