a79d7d955027095878ff88d3efd5bc6834757be3
shangrex <hshangrex@gmail.com>
2025-02-01 21:06:21 -0600

    Time: 0 ms (100%), Space: 12 MB (63.78%) - LeetHub

--- 0419-battleships-in-a-board/0419-battleships-in-a-board.cpp
+++ 0419-battleships-in-a-board/0419-battleships-in-a-board.cpp
@@ -1,12 +1,22 @@
 class Solution {
 public:
-    void dfs(vector<vector<char>>& board, int i, int j, int rst){
+    // void dfs(vector<vector<char>>& board, int i, int j, int rst){
+    //     board[i][j] = '.';
+    //     int m = board.size(), n = board[0].size();
+    //     if(i-1>-1 && board[i-1][j] == 'X') dfs(board, i-1, j, rst);
+    //     if(j-1>-1 && board[i][j-1] == 'X') dfs(board, i, j-1, rst);
+    //     if(i+1<m && board[i+1][j] == 'X') dfs(board, i+1, j, rst);
+    //     if(j+1<n && board[i][j+1] == 'X') dfs(board, i, j+1, rst);
+    // }
+
+    void dfs(vector<vector<char>>& board, int i, int j){
+        if(i < 0 || j < 0 || i >= board.size() || j >= board[0].size()) return;
+        if(board[i][j] == '.') return;
         board[i][j] = '.';
-        int m = board.size(), n = board[0].size();
-        if(i-1>-1 && board[i-1][j] == 'X') dfs(board, i-1, j, rst);
-        if(j-1>-1 && board[i][j-1] == 'X') dfs(board, i, j-1, rst);
-        if(i+1<m && board[i+1][j] == 'X') dfs(board, i+1, j, rst);
-        if(j+1<n && board[i][j+1] == 'X') dfs(board, i, j+1, rst);
+        dfs(board, i-1, j);
+        dfs(board, i, j-1);
+        dfs(board, i+1, j);
+        dfs(board, i, j+1);
     }
     int countBattleships(vector<vector<char>>& board) {
         int rst = 0;
@@ -16,7 +26,7 @@ public:
             for(int j = 0; j < n; j++){
                 if(board[i][j] == 'X'){
                     rst++;
-                    dfs(board, i, j, rst);
+                    dfs(board, i, j);
                 }
             }
         }
