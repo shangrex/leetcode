456f9138942d8d76075e16c6d2381311fd6e058d
shangrex <hshangrex@gmail.com>
2025-02-02 16:03:24 -0600

    Time: 271 ms (5.01%), Space: 8.5 MB (92.19%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
-    bool isPalindrome(int x) {
+    /*
+    1. Convert to string
+        bool isPalindrome(int x) {
         if(x < 0) return false;
         string str_num = to_string(x);
         int l = 0, r = str_num.length()-1;
@@ -12,5 +14,23 @@ public:
             r--;
         }
         return true;
+    }
+    2. 
+    */
+    bool isPalindrome(int x) {
+        if(x < 0) return false;
+        // revert and compare
+        long long cnt_dig = 1, cnt = 0;
+        while(cnt_dig <= x) {cnt_dig*=10;cnt++;}
+        cout << x << endl;
+        long long t = 0, y=x;
+        for(int i = 0; i < cnt; i++){
+            t*=10;
+            t+=(x%10);
+            x/=10;
+            cout << t<<endl;
+        }
+        cout << t << endl;
+        return t==y;
     }
 };
