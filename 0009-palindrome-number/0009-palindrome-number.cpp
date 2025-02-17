d86156be5bf053980678f1d3346471e146d564b7
shangrex <hshangrex@gmail.com>
2025-02-17 17:35:49 -0600

    Time: 0 ms (100%), Space: 8.6 MB (41.63%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -16,18 +16,30 @@ public:
         return true;
     }
     2. 
+        if(x < 0) return false;
+    // revert and compare
+    long long cnt_dig = 1, cnt = 0;
+    while(cnt_dig <= x) {cnt_dig*=10;cnt++;}
+    long long t = 0, y=x;
+    for(int i = 0; i < cnt; i++){
+        t*=10;
+        t+=(x%10);
+        x/=10;
+    }
+    return t==y;
+
+
     */
     bool isPalindrome(int x) {
-        if(x < 0) return false;
-        // revert and compare
-        long long cnt_dig = 1, cnt = 0;
-        while(cnt_dig <= x) {cnt_dig*=10;cnt++;}
-        long long t = 0, y=x;
-        for(int i = 0; i < cnt; i++){
-            t*=10;
-            t+=(x%10);
+        if(x < 0 || (x%10 == 0 && x != 0)) return false;
+        int rev_num = 0;
+        // #count digit
+        // transform the tmp
+        while(rev_num < x){
+            rev_num = x%10+rev_num*10;
             x/=10;
         }
-        return t==y;
+        return rev_num == x || rev_num/10 == x;
+
     }
 };
