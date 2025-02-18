3d8f402f159545e0b61d3ee63950e3edd2a1f1f1
shangrex <hshangrex@gmail.com>
2025-02-18 16:48:21 -0600

    Time: 8 ms (11.11%), Space: 19.7 MB (15.64%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,10 +1,7 @@
 class Solution {
 public:
     /*
-    
-    */
-    vector<int> topKFrequent(vector<int>& nums, int k) {
-        priority_queue<pair<int, int>> pq;
+            priority_queue<pair<int, int>> pq;
         unordered_map<int, int>mp;
         for(auto num : nums){
             if(mp.find(num) != mp.end()){
@@ -23,6 +20,53 @@ public:
             rst.push_back(p.second);
             pq.pop();
         }
+        return rst;
+    */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /*
+    Approach 1. map
+    Approach 2. bucket sort
+    */
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int, int>mp;
+        for(auto num : nums){
+            mp[num] += 1;
+        }
+
+        int n = nums.size();
+        vector<vector<int>> bucket(n+1);
+        for(auto it : mp){
+            bucket[it.second].push_back(it.first);
+        }
+        int cnt_k = 0;
+        vector<int> rst;
+        for(int i = n; i >= 0; i--){
+            if(!bucket[i].empty()){
+                for(int j = 0; j < bucket[i].size(); j++){
+                    rst.push_back(bucket[i][j]);
+                    cnt_k++;
+                }
+            }
+            if(cnt_k >= k) break;
+        }
+
         return rst;
     }
 };
