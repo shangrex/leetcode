e01bc32257e71187de61f44ab472a990923eeb1f
shangrex <hshangrex@gmail.com>
2025-02-19 14:26:23 -0600

    Time: 0 ms (100%), Space: 8.7 MB (83.65%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
-    bool isValid(string s) {
+    /*
+    Approach 1.
+     bool isValid(string s) {
         stack<char> st;
         
         for(int i = 0; i < s.length(); i++){
@@ -28,5 +30,34 @@ public:
         }
         if(!st.empty())return false;
         return true;
+    }
+    */
+    bool isValid(string s) {
+        stack<char> st;
+        
+        for(char c : s){
+            if(c == ')'){
+                if(!st.empty() && st.top() == '('){
+                    st.pop();
+                }
+                else return false;
+            }
+            else if(c == ']'){
+                if(!st.empty() && st.top() == '['){
+                    st.pop();
+                }
+                else return false;
+            }
+            else if(c == '}'){
+                if(!st.empty() && st.top() == '{'){
+                    st.pop();
+                }
+                else return false;
+            }
+            else {
+                st.push(c);
+            }
+        }
+        return st.empty();
     }
 };
