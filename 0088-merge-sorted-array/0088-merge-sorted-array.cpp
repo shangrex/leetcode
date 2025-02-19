2e5a351b6611e73c24b417f2dde28560d1f99afb
shangrex <hshangrex@gmail.com>
2025-02-19 15:20:16 -0600

    Time: 0 ms (100%), Space: 12.3 MB (70.63%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -4,7 +4,6 @@ public:
         int p1 = m-1;
         int p2 = n-1;
         for(int p = m+n-1; p >= 0; p--){
-            cout << p << " " << p1 << " " << p2 << endl;
             if(p2 < 0)break;
             if(p1 >=0 && nums1[p1] > nums2[p2]){
                 nums1[p] = nums1[p1--];
