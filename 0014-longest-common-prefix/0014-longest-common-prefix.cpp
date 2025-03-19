c7f64b68037567f9008ce682e535b31b7861b229
shangrex <hshangrex@gmail.com>
2025-03-19 10:33:16 -0500

    Time: 0 ms (100%), Space: 11.7 MB (95.41%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if(strs.size() == 1)return strs[0];
+        int ret = 0;
+
+        while(true){
+            bool flag = true;
+            for(int i = 0; i < strs.size()-1; i++){
+                if(ret >= strs[i].length() || ret >= strs[i+1].length()) {
+                    flag = false;
+                    break;
+                }
+                if(strs[i][ret] != strs[i+1][ret]){
+                    flag = false;
+                    break;
+                }
+            }
+            if(!flag)break;
+            else ret++;
+        }
+  
+
+        return strs[0].substr(0, ret);
+    }
+};
