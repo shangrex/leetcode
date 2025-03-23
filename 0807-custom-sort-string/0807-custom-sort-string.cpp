b46acafd1aed7001337bd2c01e8e0e15ce41d45d
shangrex <hshangrex@gmail.com>
2025-03-23 17:30:25 -0500

    Time: 0 ms (100%), Space: 8.2 MB (89.54%) - LeetHub

--- 0807-custom-sort-string/0807-custom-sort-string.cpp
+++ 0807-custom-sort-string/0807-custom-sort-string.cpp
@@ -10,14 +10,7 @@ public:
     use hashtable to count the index of the char + sort in lambda and use &
     !! crucial !!
     sort need to consider any condition in lambda
-    T: O(NlgN)
-    S: number of char in order
-
-
-    Approach 2. 
-    use 
-    */
-    string customSortString(string order, string s) {
+string customSortString(string order, string s) {
         unordered_map<char, int>mp;
         
         for(int i = 0; i < order.length(); i++){
@@ -44,6 +37,34 @@ public:
         
         });
 
+        return s;
+    }
+
+
+    !! follow up !!
+    can use string find 
+    T: O(NlgN)
+    S: O(N) in C++ sorting
+
+    std::string::npos is an unsigned “largest possible size_t” value.
+    npos means all bits set to 1 
+
+    string customSortString(string order, string s) {
+        // std::string::npos is an unsigned “largest possible size_t” value.
+        sort(s.begin(), s.end(), [&](char i, char j){
+            return order.find(i) < order.find(j);
+        });
+        return s;
+    }
+
+    Approach 2. 
+    use 
+    */
+    string customSortString(string order, string s) {
+        // std::string::npos is an unsigned “largest possible size_t” value.
+        sort(s.begin(), s.end(), [&](char i, char j){
+            return order.find(i) < order.find(j);
+        });
         return s;
     }
 };
