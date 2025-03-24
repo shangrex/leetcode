6f79c6fb45864e31dbf57ca531bd1a0f82b92862
shangrex <hshangrex@gmail.com>
2025-03-23 20:01:20 -0500

    Time: 0 ms (100%), Space: 15.7 MB (33.16%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    /*
+    the permuation list is stricly increasing
+    123
+    132
+    213
+    231
+    312
+    321
+
+    The biggest permutation is 3 2 1 strictly decreasing.
+
+
+    Approach 1.
+    First scan from the end of the nums,
+        Find the frist decreasing number from the right. Let's say ith index
+    Second scan from the ith to the right. Find the smallest number that larger than nums[i]
+
+    Third reverse i+1
+
+    T: O(N)
+    S: O(1)
+    */
+    void nextPermutation(vector<int>& nums) {
+        int i = nums.size()-2;
+        while(i >= 0 && nums[i] >= nums[i+1]){
+            i--;
+        }
+        
+        if(i >= 0){
+            int j = nums.size()-1;
+            while(j >= 0 && nums[j] <= nums[i]){
+                j--;
+            }
+            
+            swap(nums[i], nums[j]);
+        }
+
+        reverse(nums.begin()+i+1, nums.end());
+    }
+};
