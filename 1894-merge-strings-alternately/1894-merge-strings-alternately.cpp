54f74ebf85146fedfdabb4e123cfef8e7faa06fb
shangrex <hshangrex@gmail.com>
2025-03-31 11:24:23 -0500

    Time: 0 ms (100%), Space: 8.3 MB (83.13%) - LeetHub

---
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        int p = 0, q = 0;
+        string ret = "";
+        while(p < word1.size() && q < word2.size()){
+            ret += word1[p];
+            ret += word2[q];
+            p++;
+            q++;
+        }
+
+        if(p < word1.size()){
+            ret += word1.substr(p);
+        }
+        else if(q < word2.size()){
+            ret += word2.substr(q);
+        }
+
+        return ret;
+    }
+};
