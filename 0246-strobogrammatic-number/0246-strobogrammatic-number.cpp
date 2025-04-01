d0acbffdeccb11af6aba877a4102d226513c5133
shangrex <hshangrex@gmail.com>
2025-03-31 19:01:14 -0500

    Time: 3 ms (0%), Space: 7.9 MB (82%) - LeetHub

--- 0246-strobogrammatic-number/0246-strobogrammatic-number.cpp
+++ 0246-strobogrammatic-number/0246-strobogrammatic-number.cpp
@@ -4,11 +4,7 @@ public:
         unordered_map<char, char>mp {{'0', '0'}, {'1', '1'}, {'6', '9'}, {'9', '6'}, {'8', '8'}};
 
         int left = 0, right = num.length()-1;
-        if(num.length() == 1){
-            if(num[0] == '1' || num[0] == '0' || num[0] == '8') return true;
-            else return false;
-        } 
-
+  
         while(left <= right){
             if(mp.find(num[left]) == mp.end()){
                 return false;
