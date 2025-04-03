195fa4fd0fa658384dbace5f330e8dc7cc5c7247
shangrex <hshangrex@gmail.com>
2025-04-02 21:05:29 -0500

    Time: 8 ms (46.84%), Space: 27.1 MB (30.22%) - LeetHub

--- 0498-diagonal-traverse/0498-diagonal-traverse.cpp
+++ 0498-diagonal-traverse/0498-diagonal-traverse.cpp
@@ -1,5 +1,11 @@
 class Solution {
 public:
+    /*
+    2, 4 are on the same diagonal, and they share the index sum of 1. 
+    (2 is matrix[0][1] and 4 is in matrix[1][0]). 3,5,7 are on the same diagonal, and they share the sum of 2. (3 is matrix[0][2], 5 is matrix[1][1], and 7 is matrix [2][0]).
+
+
+    */
     vector<int> findDiagonalOrder(vector<vector<int>>& mat) {
         int m = mat.size();
         int n= mat[0].size();
