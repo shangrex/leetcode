2f133eb8e588b28d17e2e20f6c3bc867b665acb3
shangrex <hshangrex@gmail.com>
2025-04-03 10:30:48 -0500

    Time: 3 ms (10.38%), Space: 8 MB (56.66%) - LeetHub

--- 0408-valid-word-abbreviation/0408-valid-word-abbreviation.cpp
+++ 0408-valid-word-abbreviation/0408-valid-word-abbreviation.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
-    bool validWordAbbreviation(string word, string abbr) {
-        int p = 0, q = 0;
+/*
+       int p = 0, q = 0;
         while(p < word.size() && q < abbr.size()){
             if(word[p] == abbr[q]){
                 p++;
@@ -22,5 +22,35 @@ public:
         }
        
         return p == word.size() && q == abbr.size();
+*/
+    bool validWordAbbreviation(string word, string abbr) {
+        int p = 0, q = 0;
+        int num = 0;
+        while(p < word.length() && q < abbr.length()){
+            // word = "internationalization", 
+            //                            p
+            // abbr = "i12iz4n"
+            //              q 
+            // cout << p << q << endl;
+            // cout << word[p] << abbr[q] << endl;
+            if(isdigit(abbr[q])){
+                if(num == 0 && abbr[q] == '0') return false;
+                num = num*10 + (abbr[q]-'0');
+                if(q == abbr.length()-1) {p += num; num =0;}
+                q++;
+            }
+            else {
+                p += num;
+                if(word[p] == abbr[q]){
+                    p++;
+                    q++;
+                }
+                else return false;
+                num = 0;
+            }
+        }
+        cout << p << " " << q << endl;
+        cout << word.length() << " " << abbr.length();
+        return p == word.length() && q == abbr.length();
     }
 };
