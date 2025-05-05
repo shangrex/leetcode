82f9ac9255390d53a7d835c1da8f9ba903556d6a
shangrex <hshangrex@gmail.com>
2025-05-05 15:23:10 -0500

    Time: 6 ms (99.24%), Space: 50.6 MB (52.94%) - LeetHub

--- 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.cpp
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.cpp
@@ -1,53 +1,111 @@
-class TrieNode {
+// class TrieNode {
+//     public:
+//     bool is_end;
+//     TrieNode *nodes[26];
+//     TrieNode(){
+//         this->is_end = false;
+//         for(int i = 0; i < 26; i++){
+//             nodes[i] = NULL;
+//         }
+//     }
+// };
+
+// class Trie {
+// public:
+//     TrieNode *root;
+//     Trie() {
+//          root = new TrieNode();
+//     }
+    
+//     void insert(string word) {
+//         TrieNode *cur = root;
+//         for(int i = 0; i < word.length(); i++){
+//             if(cur->nodes[word[i] - 'a'] == NULL){
+//                 cur->nodes[word[i]-'a'] = new TrieNode();                
+//             }
+//             cur = cur->nodes[word[i]-'a'];
+//         }
+//         cur->is_end = true;
+//     }
+    
+//     bool search(string word) {
+//         TrieNode*cur = root;
+//         for(int i = 0; i < word.length(); i++){
+//             if(cur->nodes[word[i]-'a'] == NULL)return false;
+//             else {
+//                 cur = cur->nodes[word[i]-'a'];
+//             }
+//         }
+//         return cur->is_end;
+//     }
+    
+//     bool startsWith(string prefix) {
+//         TrieNode* cur = root;
+//         for(int i = 0; i < prefix.length(); i++){
+//             if(cur->nodes[prefix[i] -'a'] == NULL)return false;
+//             cur = cur->nodes[prefix[i]-'a'];
+//         }
+//         return true;
+//     }
+// };
+
+class TrieNode{
     public:
+    TrieNode *node[26];
     bool is_end;
-    TrieNode *nodes[26];
     TrieNode(){
         this->is_end = false;
         for(int i = 0; i < 26; i++){
-            nodes[i] = NULL;
+            node[i] = NULL;
         }
     }
 };
 
 class Trie {
-public:
+    public:
+
     TrieNode *root;
-    Trie() {
-         root = new TrieNode();
+    Trie(){
+        root = new TrieNode();
     }
-    
-    void insert(string word) {
+
+    void insert(string word){
         TrieNode *cur = root;
         for(int i = 0; i < word.length(); i++){
-            if(cur->nodes[word[i] - 'a'] == NULL){
-                cur->nodes[word[i]-'a'] = new TrieNode();                
+            // cout << word[i];
+            if(cur->node[word[i]-'a'] == NULL){
+                cur->node[word[i]-'a'] = new TrieNode();
             }
-            cur = cur->nodes[word[i]-'a'];
+            cur = cur->node[word[i]-'a'];
         }
         cur->is_end = true;
     }
-    
-    bool search(string word) {
-        TrieNode*cur = root;
+
+    bool search(string word){
+        TrieNode *cur = root;
         for(int i = 0; i < word.length(); i++){
-            if(cur->nodes[word[i]-'a'] == NULL)return false;
-            else {
-                cur = cur->nodes[word[i]-'a'];
+            if(cur->node[word[i] - 'a'] == NULL){
+                return false;
             }
+            cur = cur->node[word[i]- 'a'];
         }
         return cur->is_end;
     }
-    
-    bool startsWith(string prefix) {
-        TrieNode* cur = root;
+
+    bool startsWith(string prefix){
+        TrieNode *cur = root;
         for(int i = 0; i < prefix.length(); i++){
-            if(cur->nodes[prefix[i] -'a'] == NULL)return false;
-            cur = cur->nodes[prefix[i]-'a'];
+            if(cur->node[prefix[i]-'a'] == NULL){
+                return NULL;
+            }
+            cur = cur->node[prefix[i]-'a'];
         }
         return true;
     }
 };
+
+
+
 
 /**
  * Your Trie object will be instantiated and called as such:
