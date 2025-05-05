479ff848ac8642f9fe10420d6cd4e9ec22b245ef
shangrex <hshangrex@gmail.com>
2025-05-04 21:32:42 -0500

    Time: 0 ms (100%), Space: 40.2 MB (79.58%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,34 +1,46 @@
+// class Solution {
+// public:
+//     vector<int> productExceptSelf(vector<int>& nums) {
+//         // be careful division zero
+//         vector<int>ans(nums.size(), 1);
+//         long long int suffix = 1;
+//         // prefix 
+//         // 1 2 3 4
+//         //   1 2 
+//         //     8 4   1
+//         //     
+//         for(int i = 1; i < nums.size(); i++){
+//             ans[i] = nums[i-1]*ans[i-1];
+//         }
+//         // suffix
+//         for(int i = nums.size()-1; i > 0; i--){
+//             suffix = suffix*nums[i];
+//             ans[i-1] = ans[i-1]*suffix;
+//         }
+//         return ans;
+//     }
+// };
 class Solution {
 public:
-    /*
-    // be careful division zero
-        vector<int>ans(nums.size(), 1);
-        long long int suffix = 1;
-        // prefix 
-        for(int i = 1; i < nums.size(); i++){
-            ans[i] = nums[i-1]*ans[i-1];
-        }
-        // suffix
-        for(int i = nums.size()-1; i > 0; i--){
-            suffix = suffix*nums[i];
-            ans[i-1] = ans[i-1]*suffix;
-        }
-        return ans;
-    */
     vector<int> productExceptSelf(vector<int>& nums) {
-        vector<int>rst(nums.size(), 1);
-        long long int suffix = 1;
-        // prifix
-        for(int i = 1 ; i < nums.size(); i++){
-            rst[i] = nums[i-1]*rst[i-1];
+        long long suffix = 1;
+        int n = nums.size();
+        vector<int>ans(n, 1);
+        // 1 2 3 4
+        //.1 1.2 6
+        //       1
+        for(int i = 1; i < n; i++){
+            ans[i] = nums[i-1] * ans[i-1];
         }
-        // suffix
-        for(int i = nums.size()-1; i > -1; i--){
-            rst[i] = rst[i] * suffix;
-            suffix = nums[i] * suffix;
+        // for(int i = 0; i < n; i++){
+        //     cout << ans[i] << " ";
+        // }
+
+        for(int i = n-1; i > 0; i--){
+            suffix *= nums[i];
+            ans[i-1] *= suffix;
         }
-        
-        return rst;
-        
+
+        return ans;
     }
 };
