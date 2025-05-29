145c3cf11ca6865083626cde4d6fc8242a57cd03
shangrex <hshangrex@gmail.com>
2025-05-30 01:49:59 +0800

    Time: 11 ms (57.61%), Space: 66.1 MB (70.43%) - LeetHub

---
+++ 3822-fill-a-special-grid/3822-fill-a-special-grid.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    /*
+    Hint: divide & conquer
+    notice that top-left -> bottom-left -> top-left -> top-right
+    */
+    int val = 0;
+    vector<vector<int>> specialGrid(int n) {
+        int size = 1 << n;
+        int rowStart = 0, rowEnd = size, colStart = 0, colEnd = size;
+        vector<vector<int>> ret(size, vector<int>(size, 0));
+        solve(ret, rowStart, rowEnd, colStart, colEnd);
+
+        return ret;
+    }
+
+private:
+    void solve(vector<vector<int>> &ret, int rowStart, int rowEnd, int colStart, int colEnd){
+        int size = rowEnd - rowStart;
+        if(size == 1){
+            ret[rowStart][colStart] = val++;
+            return ;
+        }
+
+        int rowMid = rowStart + (rowEnd - rowStart)/2;
+        int colMid = colStart + (colEnd - colStart)/2;
+        solve(ret, rowStart, rowMid, colMid, colEnd); // top-right
+        solve(ret, rowMid, rowEnd, colMid, colEnd); // bottom-right
+        solve(ret, rowMid, rowEnd, colStart, colMid); // bottom-left
+        solve(ret, rowStart, rowMid, colStart, colMid); // top-left
+
+    }
+
+};
