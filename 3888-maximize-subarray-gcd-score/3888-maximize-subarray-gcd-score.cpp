270cd392910124d68b009e3b3234aff61a2c568a
shangrex <hshangrex@gmail.com>
2025-06-19 19:08:41 +0800

    Time: 872 ms (55.04%), Space: 38 MB (65.73%) - LeetHub

--- 3888-maximize-subarray-gcd-score/3888-maximize-subarray-gcd-score.cpp
+++ 3888-maximize-subarray-gcd-score/3888-maximize-subarray-gcd-score.cpp
@@ -4,14 +4,17 @@ public:
 // 0 1 0
 // l
 // 3
-//     r
+//   
     long long maxGCDScore(vector<int>& nums, int k) {
-
+        /*
+        Time complexity: 
+        */
         int n = nums.size();
         long long ret = 0;
 
         // count power of two
         vector<int> power2Vector(n);
+        // O(N)
         for(int i = 0; i < n; i++){
             int tmp = nums[i];
             while(tmp % 2 == 0){
@@ -19,6 +22,7 @@ public:
                 tmp/=2;
             }
         }
+        // O(N^2 * log(A)) for gcd 
         for(int i = 0 ; i < n; i++){
             int cntPower2 = 0, gcdValue = 0, power2 = INT_MAX;
             for(int j = i; j < n; j++){
@@ -30,10 +34,10 @@ public:
                     cntPower2++;
                 }
                 if(cntPower2 <= k){
-                    ret = max(ret, (long long)((j - i + 1) * 2 * (long long)gcdValue));
+                    ret = max(ret, 1LL * ((j - i + 1) * 2 * (long long)gcdValue));
                 }
                 else {
-                    ret = max(ret, (long long)((j - i + 1) *(long long)gcdValue));
+                    ret = max(ret, 1LL * ((j - i + 1) *(long long)gcdValue));
                 }
             }
 
