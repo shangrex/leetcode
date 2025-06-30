6c0fc1921154ee0e940171d605d8b44d75918679
shangrex <hshangrex@gmail.com>
2025-06-30 16:36:46 +0800

    Time: 0 ms (100%), Space: 14.6 MB (90.04%) - LeetHub

--- 2320-find-all-k-distant-indices-in-an-array/2320-find-all-k-distant-indices-in-an-array.cpp
+++ 2320-find-all-k-distant-indices-in-an-array/2320-find-all-k-distant-indices-in-an-array.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     vector<int> findKDistantIndices(vector<int>& nums, int key, int k) {
+        /*
+        Appraoch 1. use an array to store
+
         vector<int> keyIndex;
         for(int i = 0; i < nums.size(); i++){
             if(nums[i] == key){
@@ -17,6 +20,21 @@ public:
             else if(trackIndex < keyIndex.size()-1 && abs(keyIndex[trackIndex+1] - i) <= k){
                 ret.push_back(i);
                 trackIndex++;
+            }
+        }
+        return ret;
+        Approach 2. two pointer
+        */
+        vector<int>ret;
+        int n = nums.size();
+        int r = 0;
+        for(int i = 0;i < n; i++){
+            if(nums[i] == key){
+                int l = max(i-k, r);
+                r = min(n-1, i+k)+1;
+                for(int j = l; j < r; j++){
+                    ret.push_back(j);
+                }
             }
         }
         return ret;
