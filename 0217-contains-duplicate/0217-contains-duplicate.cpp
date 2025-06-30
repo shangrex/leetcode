df93527c50dd7bcc52874932285306376b919f85
shangrex <hshangrex@gmail.com>
2025-06-30 21:55:16 +0800

    Time: 56 ms (33.11%), Space: 90.8 MB (46.25%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,25 +1,13 @@
 class Solution {
 public:
-    /*
-    unordered_map<int, bool>mp;
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int, bool>mp;
         for(int i = 0; i < nums.size(); i ++){
             // not exist
             if(mp.find(nums[i]) == mp.end()){
                 mp[nums[i]] = true;
             }
             else return true;
-        }
-        return false;
-    */
-    bool containsDuplicate(vector<int>& nums) {
-        unordered_map<int, bool>mp;
-        for(auto i : nums){
-            //exist
-            if(mp.find(i) != mp.end()){
-                return true;
-            }
-            // not exist
-            else mp[i] = true;
         }
         return false;
     }
