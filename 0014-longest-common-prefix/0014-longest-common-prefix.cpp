21818357608954b6a82240dcd24f8bb45e0ca601
shangrex <hshangrex@gmail.com>
2025-06-30 22:58:47 +0800

    Time: 0 ms (100%), Space: 11.9 MB (66.5%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -26,13 +26,26 @@ public:
 
         return strs[0].substr(0, ret);
     }
-    */
-    string longestCommonPrefix(vector<string>& strs) {
-        if(strs.size() == 1)return strs[0];
+
+
+           if(strs.size() == 1)return strs[0];
 
         for(int i = 0; i < strs[0].size(); i++){
             for(int j = 1; j < strs.size(); j++){
                 if(i == strs[j].size() || strs[0][i] != strs[j][i]){
+                    return strs[0].substr(0, i);
+                }
+            }
+        }
+
+        return strs[0];
+    */
+    string longestCommonPrefix(vector<string>& strs) {
+        if(strs.size() == 1) return strs[0];
+
+        for(int i = 0; i < strs[0].size(); i++){
+            for(int j = 1; j < strs.size(); j++){
+                if(strs[j][i] != strs[0][i]){
                     return strs[0].substr(0, i);
                 }
             }
