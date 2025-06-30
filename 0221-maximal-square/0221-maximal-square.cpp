2c275f90ca0006d24ae8cb1430570243de9a8071
shangrex <hshangrex@gmail.com>
2025-06-30 20:15:06 +0800

    Time: 35 ms (12.6%), Space: 31 MB (58.24%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.cpp
+++ 0221-maximal-square/0221-maximal-square.cpp
@@ -1,12 +1,20 @@
 class Solution {
 public:
+    // int recursive(int i, int j,vector<vector<char>>&matrix, vector<vector<int>> &memo){
+    //     if(i >= matrix.size() || j >= matrix[0].size() || i < 0 || j < 0) return 0;
+    //     if(matrix[i][j] == '0') return memo[i][j];
+    //     if(memo[i][j] > 0) return memo[i][j];
+    //     return memo[i][j] = min({recursive(i-1, j, matrix, memo),
+    //                         recursive(i, j-1, matrix, memo), 
+    //                         recursive(i-1, j-1, matrix, memo)})+1;
+    // }
     int recursive(int i, int j,vector<vector<char>>&matrix, vector<vector<int>> &memo){
         if(i >= matrix.size() || j >= matrix[0].size() || i < 0 || j < 0) return 0;
         if(matrix[i][j] == '0') return memo[i][j];
         if(memo[i][j] > 0) return memo[i][j];
-        return memo[i][j] = min({recursive(i-1, j, matrix, memo),
-                            recursive(i, j-1, matrix, memo), 
-                            recursive(i-1, j-1, matrix, memo)})+1;
+        return memo[i][j] = min({recursive(i+1, j, matrix, memo),
+                            recursive(i, j+1, matrix, memo), 
+                            recursive(i+1, j+1, matrix, memo)})+1;
     }
     int maximalSquare(vector<vector<char>>& matrix) {
         int n = matrix.size();
