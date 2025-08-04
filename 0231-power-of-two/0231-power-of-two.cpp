14995f8a0a20bdf671287980c00cfba8a026c62f
shangrex <hshangrex@gmail.com>
2025-08-04 11:30:22 +0800

    Time: 0 ms (100%), Space: 7.7 MB (98.54%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -5,13 +5,23 @@ public:
     // 4 100
     bool isPowerOfTwo(int n) {
         /*
-        count the 1 bit of the number, if the count equal 1 than return true
-        */
+        Approach 1. count the 1 bit of the number, if the count equal 1 than return true.
+        time lg2
         if(n <= 0) return false;
         while(n){
             if(n&1 && n > 1) return false;
             n/=2;
         }
         return true;
+        Approach 2. bitwise operation
+        n & -n -> all bit are reverted except the rightmost 1 bit
+        5
+        0101
+        -5
+        1011
+
+        */
+        if(n <= 0) return false;
+        return (n & -n) == n;
     }
 };
