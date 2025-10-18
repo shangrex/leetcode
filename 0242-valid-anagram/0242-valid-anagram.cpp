cee434c078c05cddf7c476fd4c74318e31035507
shangrex <hshangrex@gmail.com>
2025-10-17 17:47:11 -0700

    Time: 4 ms (38.65%), Space: 9.8 MB (48.12%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -7,16 +7,12 @@ public:
         */
 
         map<char, int>mp;
-        map<char, int>mp2;
         for(auto i : s){
             mp[i]++;
         }
         for(auto i : t){
-            //mp[i]--;
-            mp2[i]++;
+            mp[i]--;
         }
-
-        return mp == mp2;
 
         for(auto i : mp){
             if(i.second != 0) return false;
