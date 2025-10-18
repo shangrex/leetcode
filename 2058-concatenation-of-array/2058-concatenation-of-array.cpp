b84b9c697fb6559469c4fd74bb4d3abe93fadcc0
shangrex <hshangrex@gmail.com>
2025-10-17 17:34:53 -0700

    Time: 0 ms (100%), Space: 16.7 MB (94.03%) - LeetHub

--- 2058-concatenation-of-array/2058-concatenation-of-array.cpp
+++ 2058-concatenation-of-array/2058-concatenation-of-array.cpp
@@ -1,5 +1,25 @@
 class Solution {
 public:
+/*
+    vector<int> getConcatenation(vector<int>& nums) {
+        int n = nums.size();
+        vector<int>ret(n*2);
+        for(int i = 0; i < n; i++){
+            ret[i] = ret[i+n] = nums[i];
+        }
+        return ret;
+    }
+*/
+
+
+
+
+
+
+
+
+
+
     vector<int> getConcatenation(vector<int>& nums) {
         int n = nums.size();
         vector<int>ret(n*2);
