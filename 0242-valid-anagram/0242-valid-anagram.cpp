faf58424794175053cffa2908a604a4149038d2a
shangrex <hshangrex@gmail.com>
2025-10-17 17:44:47 -0700

    Time: 4 ms (38.65%), Space: 9.7 MB (74.12%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -6,13 +6,17 @@ public:
         Approach2. use a unordered_map to balance the count
         */
 
-        unordered_map<char, int>mp;
+        map<char, int>mp;
+        map<char, int>mp2;
         for(auto i : s){
             mp[i]++;
         }
         for(auto i : t){
-            mp[i]--;
+            //mp[i]--;
+            mp2[i]++;
         }
+
+        return mp == mp2;
 
         for(auto i : mp){
             if(i.second != 0) return false;
