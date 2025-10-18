d84985c367a356d1ce61dd928d7939cd23ba8eb3
shangrex <hshangrex@gmail.com>
2025-10-17 17:37:32 -0700

    Time: 0 ms (100%), Space: 9.9 MB (11.21%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        int s_cnt[26], c_cnt[26];
-        for(int i = 0; i < 26; i++){
-            s_cnt[i] = 0;
-            c_cnt[i] = 0;
+        /*
+        Approah 1. use two size of 26 vector to store count
+        Approach2. use a unordered_map to balance the count
+        */
+
+        unordered_map<char, int>mp;
+        for(auto i : s){
+            mp[i]++;
         }
-        
-        for(int i = 0; i < s.length(); i++){
-            s_cnt[s[i]-'a'] += 1;
+        for(auto i : t){
+            mp[i]--;
         }
-        
-        for(int i = 0; i < t.length(); i++){
-            c_cnt[t[i]-'a'] += 1;
-        }
-        
-        for(int i = 0; i < 26; i++){
-            if(s_cnt[i] != c_cnt[i]) return false;
+
+        for(auto i : mp){
+            if(i.second != 0) return false;
         }
         return true;
-
     }
 };
