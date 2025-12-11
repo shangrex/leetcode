cd1ef6e8c1ae09943ce7e5efeb0ca28e1588f32d
shangrex <hshangrex@gmail.com>
2025-12-10 22:53:26 -0800

    Time: 0 ms (100%), Space: 97.3 MB (60.45%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,32 +1,13 @@
 class Solution {
 public:
-    /*
-    Approach 1:
-    Greedy approach
-    to see the best profit, we must buy the current min stock and sell it
-    so traverse the vector and sell in each index, and store the min prices 
-    
-    Approach 2:
-    sliding window
-    left pointer buy stock
-    right pointer sell stock
-    if left pointer is smaller move left
-    if right pointer is bigger move right
-    otherwise move left
-    */
     int maxProfit(vector<int>& prices) {
-        int rst = 0;
-        int left = 0;
-        int right = left+1;
-        while(right < prices.size()){
-            rst = max(rst, prices[right]-prices[left]);
-            if(prices[right] < prices[left]){
-                left = right;
-            }
-            else{
-                right++;
-            }
+        int min_buy = prices[0];
+        int ret = 0;
+        for(auto &p : prices){
+            int t = p - min_buy;
+            ret = max(ret, t);
+            min_buy = min(min_buy, p);
         }
-        return rst;
+        return ret;
     }
 };
