9008b29576aa8436c355d221a98fc3b0cae5ef31
shangrex <hshangrex@gmail.com>
2025-12-12 19:34:45 -0800

    Time: 4 ms (9.53%), Space: 15.7 MB (9.25%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,43 +1,38 @@
 class Solution {
 public:
-    /*
-    void backtrack(int index, vector<int>&nums, vector<int>&pool, vector<vector<int>>&rst){
-        rst.push_back(pool);
-        for(int i = index; i < nums.size(); i++){
-            pool.push_back(nums[i]);
-            backtrack(i+1, nums, pool, rst);
-            pool.pop_back();
-        }
-    }
-    
-    vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>>rst;
-        vector<int>pool;
-        
-        backtrack(0, nums, pool, rst);
-        return rst;
-    }
-    */
-    void backtrack(int index, vector<int>&nums, vector<int>&pool, vector<vector<int>>&rst){
-        if(index == nums.size()){
-            rst.push_back(pool);
+  
+    void backtrack(vector<vector<int>> &ret, vector<int>&nums, vector<int>tmp, int idx){
+        if(idx >= nums.size()){
+            ret.push_back(tmp);
             return;
         }
-        pool.push_back(nums[index]);
-        backtrack(index+1, nums, pool, rst);
-        pool.pop_back();
-        backtrack(index+1, nums, pool, rst);
+        backtrack(ret, nums, tmp, idx+1);
+        tmp.push_back(nums[idx]);
+        backtrack(ret, nums, tmp, idx+1);
+        tmp.pop_back();
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> ret;
+        vector<int> tmp;
+        int idx = 0;
+        backtrack(ret, nums, tmp, idx);
+        return ret;
     }
     
-    vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>>rst;
-        vector<int>pool;
-        rst.push_back(pool);
-        for(int i = 0; i < nums.size(); i++){
-            pool.push_back(nums[i]);
-            backtrack(i+1, nums, pool, rst);
-            pool.pop_back();
-        }
-        return rst;
-    }
+
+    // void backtrack(int idx, vector<vector<int>>&rst, vector<int>&nums, vector<int>&pool){
+    //     if(idx == nums.size())rst.push_back(pool);
+    //     for(int i = idx; i < nums.size(); i++){
+    //         pool.push_back(nums[i]);
+    //         backtrack(idx+1, rst, nums, pool);
+    //         pool.pop_back();
+    //         backtrack(idx+1, rst, nums, pool);
+    //     }
+    // }
+    // vector<vector<int>> subsets(vector<int>& nums) {
+    //     vector<vector<int>> rst;
+    //     vector<int>pool;
+    //     backtrack(0, rst, nums, pool);
+    //     return rst;
+    // }
 };
