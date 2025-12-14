b9f9dde198736fc68536523deb6e198b6d7fec78
shangrex <hshangrex@gmail.com>
2025-12-13 17:32:58 -0800

    Time: 3 ms (63.74%), Space: 18.8 MB (27.65%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -10,71 +10,27 @@
  */
 class Solution {
 public:
-    /*
-    Approach 1. merge of two sorted array
-    Approach 2. priroty_queue the each list first element and iterate
-    */
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        ListNode *dummy = new ListNode();
-        priority_queue<pair<int, ListNode*>, vector<pair<int, ListNode*>>, greater<pair<int, ListNode*>> >pq;
-        for(int i = 0; i < lists.size(); i++){
-            if(lists[i])
-                pq.push({lists[i]->val, lists[i]});
+        priority_queue<pair<int, ListNode*>, vector<pair<int, ListNode*>>, greater<pair<int, ListNode*>> > q;
+
+        for(auto &l : lists){
+            if(l) q.push({l->val, l});
         }
-        
-        //dummy->next = pq.top().second;
-        ListNode*cur = dummy;
-        while(!pq.empty()){
-            pair<int, ListNode*> tp = pq.top();
-            cur->next = tp.second;
+
+        ListNode *dummy = new ListNode(0);
+        ListNode *cur;
+        cur = dummy;
+        while(!q.empty()){
+            pair<int, ListNode*> point = q.top();
+            q.pop();
+            cur->next = point.second;
             cur = cur->next;
-            pq.pop();
-            ListNode* nxt = tp.second->next;
-            if(nxt) pq.push({nxt->val, nxt});
-       
+
+            if(point.second->next){
+                ListNode *nxt = point.second->next;
+                q.push({nxt->val, nxt});
+            }
         }
-        
-        
         return dummy->next;
-        
-        // Approach 1.
-        // int intervals = lists.size();
-        // int amount = 1;
-        // while(amount < intervals){
-        //     for(int i = 0; i < lists.size()-amount; i+=amount*2){
-        //         // cout << "list merge" << i << i+amount << endl;
-        //         lists[i] = merge2Lists(lists[i], lists[i+amount]);
-        //     }
-        //     amount*=2;
-        // }
-        // if(lists.size() == 0)return NULL;
-        // else return lists[0];
-    }
-private:
-    ListNode* merge2Lists(ListNode* l1, ListNode* l2){
-        ListNode*rst = new ListNode();
-        ListNode*cur = rst;
-        while(l1 && l2){
-            // cout << l1->val << " " << l2->val << endl;
-            if(l1->val > l2->val){
-                cur->next = l2;
-                l2 = l2->next;
-                cur = cur->next;
-            }
-            else {
-                cur->next = l1;
-                l1 = l1->next;
-                cur = cur->next;
-            }
-        }
-        
-        if(l1){
-            cur->next = l1;
-        }
-        else {
-            cur->next = l2;
-        }
-        
-        return rst->next;
     }
 };
