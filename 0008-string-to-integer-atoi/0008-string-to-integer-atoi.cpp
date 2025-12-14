5a7ee06ecea326c6a15abc7d7266728b37135a99
shangrex <hshangrex@gmail.com>
2025-12-13 16:36:01 -0800

    Time: 7 ms (3.63%), Space: 9.1 MB (54.97%) - LeetHub

---
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int myAtoi(string s) {
+        int idx = 0;
+        int sign = 1;
+        while(s[idx] == ' ') idx++;
+        if(s[idx] == '+'){
+            sign = 1;
+            idx++;
+        }
+        else if(s[idx] == '-'){
+            sign = -1;
+            idx++;
+        }
+
+        int base = 0;
+        long long accNum = 0, unitDigit = 0;
+        // cout << s[idx] << idx  << endl;
+        cout << sign << endl;
+        while(isdigit(s[idx])){
+            if(accNum > INT_MAX/10 || (accNum == INT_MAX/10 && s[idx] > '7') 
+                || accNum * sign < INT_MIN/10 || (accNum == INT_MIN/10 && s[idx] > '7') ){
+              if(sign > 0) {cout << 'f' << endl;return INT_MAX;}
+                else {cout << 'b' << endl; return INT_MIN;}
+            }
+            accNum = 10 * accNum + (s[idx] - '0');
+            idx++;
+        }
+        cout << 'r' << accNum << endl;
+        return accNum * sign;
+    }
+};
