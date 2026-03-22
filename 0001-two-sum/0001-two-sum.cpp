1817bc007d55148d15dc3b8356fdeb922f3ff7cc
shangrex <hshangrex@gmail.com>
2026-03-21 22:40:35 -0700

    Time: 1 ms (76.84%), Space: 14.8 MB (52.32%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -24,11 +24,9 @@ public:
 
 
         unordered_map<int, int> mp;
-
         for(int i = 0; i < nums.size(); i++){
-            int remain = target - nums[i];
-            if(mp.find(remain) != mp.end()){
-                return {i, mp[remain]};
+            if(mp.find(target-nums[i]) != mp.end()){
+                return {mp[target-nums[i]], i};
             }
             mp[nums[i]] = i;
         }
