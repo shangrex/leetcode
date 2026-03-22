923924fdc5023923d035e10321432e359bc2e1fd
shangrex <hshangrex@gmail.com>
2026-03-22 11:08:40 -0700

    Time: 6 ms (66.08%), Space: 11.9 MB (55.22%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    int lengthOfLongestSubstring(string s) {
+    /*
+        int lengthOfLongestSubstring(string s) {
         int left = 0, right = 0;
         unordered_map<char, int> mp;
         int ret = 0;
@@ -12,6 +13,26 @@ public:
                 }
             }
             ret = max(ret, right - left +1);
+            right++;
+        }
+        return ret;
+    }
+    */
+    int lengthOfLongestSubstring(string s) {
+        int left = 0, right = 0;
+        unordered_map<char, int> mp;
+        int ret = 0;
+        while(right < s.length()){
+            mp[s[right]]++;
+
+            while(mp[s[right]] > 1){
+                if(mp[s[left]] > 0){
+                    mp[s[left]]--;
+                }
+                else mp.erase(s[left]);
+                left++;
+            }
+            ret = max(ret, right-left+1);
             right++;
         }
         return ret;
