9b29b03b3c4f88b54a8515b20733150d5da79af5
shangrex <hshangrex@gmail.com>
2026-05-15 13:23:50 -0700

    Time: 4 ms (49.96%), Space: 118.1 MB (63.76%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -12,23 +12,56 @@ class Solution {
 public:
     /*
     First approach array + two pointer
-    Second Reverse Linked list
-    */
     bool isPalindrome(ListNode* head) {
         ListNode *cur = head;
         vector<int> arr;
         while(cur){
-            arr.push_back(cur->val);
-            cur = cur->next;
+…        }
+        return true;
+    }
+    Second Reverse Linked list
+    */
+    ListNode* reverseList(ListNode* cur){
+        ListNode *tmpNode = NULL, *prev = NULL;
+        
+        while(cur){
+            /*
+            null -> 1 -> 2 -> 3
+            t.     c
+            */
+            tmpNode = cur->next;
+            cur->next = prev;
+            prev = cur;
+            cur = tmpNode;
         }
-        int l = 0, r = arr.size()-1;
-        while(l < r){
-            if(arr[l] != arr[r]){
+
+        return prev;
+    }
+    bool isPalindrome(ListNode* head){
+
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(fast && fast->next){
+            fast = fast->next->next;
+            slow = slow->next;
+        }
+        ListNode *rList = reverseList(slow);
+        ListNode *cur = head;
+
+        // while(rList){
+        //     cout << rList->val << endl;
+        //     rList = rList->next;
+        // }
+        while(cur && rList){
+            if(cur->val != rList->val){
+                cout << "c" << cur->val << endl;
+                cout << rList->val << endl;
                 return false;
             }
-            l++;
-            r--;
+            cur = cur->next;
+            rList = rList->next;
         }
         return true;
+        
     }
 };
