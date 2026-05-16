555ed2e31814a59688c274efb26513420a58a4a6
shangrex <hshangrex@gmail.com>
2026-05-16 00:54:28 -0700

    Time: 193 ms (5.03%), Space: 91.3 MB (9.33%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+/*
     int longestConsecutive(vector<int>& nums) {
         unordered_set<int> st;
         for(int i = 0; i < nums.size(); i++){
@@ -18,6 +19,29 @@ public:
                 ret = max(ret, streak);
             }
             
+        }
+        return ret;
+*/
+
+    set<int>s;
+
+
+    int longestConsecutive(vector<int>& nums) {
+        int ret = 0;
+        for(auto &n : nums){
+            s.insert(n);
+        }
+
+        for(auto i: s){
+            if(!s.count(i-1)){
+                int numStrike = 1;
+                int numTrack = i;
+                while(s.count(numTrack+1) > 0){
+                    numStrike++;
+                    numTrack++;
+                }
+                ret = max(ret, numStrike);
+            }
         }
         return ret;
     }
