4e97f664104e3b5561a136580c0c5913b6869d15
shangrex <hshangrex@gmail.com>
2026-06-09 12:28:53 -0700

    Time: 70 ms (95.18%), Space: 44.4 MB (52.05%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.cpp
+++ 0743-network-delay-time/0743-network-delay-time.cpp
@@ -1,46 +1,40 @@
 class Solution {
 public:
-    void dfs(vector<vector<pair<int, int>>> &graph, vector<int>&timed, int cost,int n,int k){
-        if(timed[k] <= cost){
-            return;
-        }
-        //cout << "visitd " << k << " " << cost << endl;
-        timed[k] = cost;
-        for(int i = 0; i < graph[k].size(); i++){
-            dfs(graph, timed, cost+graph[k][i].second, n, graph[k][i].first);
-        }
-    }
+    /*
     
+    */
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        /*
-        Approach 1. DFS
-        Approach 2. BFS
-        Ususally use for unweited graph
-        Approach 3. Dijkstra
-        Usually use for shortest path in non-negative weights
-        Approach 4. Bellmon Fod
-        Can find shortest path in negative wieght
-        */
-        vector<vector<pair<int, int>>> graph(n+1);
-        vector<bool>visited(n, true);
-        vector<int> timed(n+1, INT_MAX);
-        
-        for(int i = 0; i < times.size(); i++){
-            graph[times[i][0]].push_back({times[i][1], times[i][2]});
-            //graph[times[i][1]].push_back({times[i][0], times[i][2]});
+        vector<vector<pair<int, int>>> graph(n+2);
+
+        for(auto& edges : times){
+            int u = edges[0];
+            int v = edges[1];
+            int w = edges[2];
+            graph[u].push_back({v, w});
         }
-        
-        // sort(graph.begin(), graph.end(), [](pair<int, int>p1, pair<int, int>p2){
-        //     return p1.second < p2.second;
-        // });
-        
-        dfs(graph, timed, 0, n, k);
-        int rst = INT_MIN;
+        vector<int> weights(n+2, INT_MAX);
+        queue<int>q;
+        q.push(k);
+        weights[k] = 0;
+        while(!q.empty()){
+            int m = q.size();
+            for(int i = 0; i < m; i++){
+                int node = q.front();
+                q.pop();
+                for (auto& t : graph[node]){
+                    int nextNode = t.first;
+                    int gap = t.second;
+                   if(weights[node] - weights[nextNode] + gap < 0){
+                        q.push(nextNode);
+                        weights[nextNode] = weights[node]+gap;
+                    }
+                }
+            }
+        }
+        int ret = INT_MIN;
         for(int i = 1; i <= n; i++){
-            //cout << timed[i] << endl;
-            rst = max(rst, timed[i]);
+            ret = max(ret, weights[i]);
         }
-        return rst == INT_MAX ? -1 : rst;
-        
+        return ret == INT_MAX ? -1 : ret;
     }
 };
