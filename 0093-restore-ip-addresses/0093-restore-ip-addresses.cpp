8fde40f7e421b9aa93a0a6a403cbeadbbc3e08aa
shangrex <hshangrex@gmail.com>
2026-06-09 16:16:27 -0700

    Time: 1 ms (47.69%), Space: 10.6 MB (67.32%) - LeetHub

--- 0093-restore-ip-addresses/0093-restore-ip-addresses.cpp
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.cpp
@@ -1,6 +1,5 @@
 class Solution {
-public:
-    
+    /*
     bool valid(string s){
         // failure case 01, 1111,
         if(s.length() < 1 || s.length() > 3){
@@ -15,7 +14,6 @@ public:
             }
         }
         return true;
-
     }
 
     void backtrack(string s, int idx, vector<string>&ret, string output, int numDot){
@@ -44,5 +42,48 @@ public:
         string output = "";
         backtrack(s, 0, ret, output, numDot);
         return ret;
+    }
+    */
+public:
+    bool valid(string t){
+        if(t.length() > 3 || t.length() < 1){
+            return false;
+        }
+        if(t[0] == '0' && t.length() > 1){
+            return false;
+        }
+        if(stoi(t) > 255){
+            return false;
+        }
+        return true;
+    }
+    void backtrack(string s,vector<string>& ret, int idx, string curStr, int numDot){
+        if(numDot == 3){
+            string tmp = s.substr(idx);
+            if(valid(tmp)){
+                curStr += tmp;
+                ret.push_back(curStr);
+            }
+            return;
+        }
+
+        for(int i = 1; i < 4; i++){
+            if(idx+i > s.length()) break;
+            string tmpStr = s.substr(idx, i);
+            // cout << idx << " " << tmpStr << endl;
+            if(valid(tmpStr)){
+                backtrack(s, ret, idx+i, curStr+tmpStr+".", numDot+1);
+            }
+        }
+    }
+    vector<string> restoreIpAddresses(string s) {
+        vector<string> ret;
+
+        int idx = 0;
+        int numDot = 0;
+        string curStr = "";
+
+        backtrack(s, ret, idx, curStr, numDot);
+        return ret;        
     }
 };
