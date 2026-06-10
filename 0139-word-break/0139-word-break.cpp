5e6cda735fd6ec0c8c0bec6fadef55bf7c13c7b5
shangrex <hshangrex@gmail.com>
2026-06-09 17:27:37 -0700

    Time: 11 ms (46.75%), Space: 16.5 MB (28.15%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,21 +1,29 @@
 class Solution {
 public:
-    bool wordBreak(string s, vector<string>& wordDict) {
-        vector<int>dp(s.size()+1, 0);
-        dp[0] = 1;
-        
-        for(int i = 1; i <= s.length(); i++){
-            for(int j = 0; j < wordDict.size(); j++){
-                int w_l = wordDict[j].length();
-                if(i >= w_l && dp[i-w_l] == 1){
-                    string tmp = s.substr(i-w_l, w_l);
-                    if(tmp == wordDict[j]){
-                        dp[i] = 1;
-                    }
-                }
+    /*
+    */
+    unordered_map<string, int> mp;
+    vector<int>dp;
+    void backtrack(string &s, int idx){
+        for(int i = 1; i+idx <= s.length(); i++){
+            string tmpStr = s.substr(idx, i);
+            if(mp.find(tmpStr) != mp.end() && dp[idx]){
+                if(dp[i+idx] == 1) continue;
+                // cout << i+idx << endl;
+                dp[i+idx] = 1;
+                backtrack(s, idx+i);
             }
         }
-        
-        return dp[s.length()];
+    }
+    bool wordBreak(string s, vector<string>& wordDict) {
+        int n = s.length();
+        dp.resize(n+2, 0);
+        for(auto &s : wordDict){
+            mp[s] = 1;
+        }
+        int idx = 0;
+        dp[0] = 1;
+        backtrack(s, idx);
+        return dp[n];
     }
 };
