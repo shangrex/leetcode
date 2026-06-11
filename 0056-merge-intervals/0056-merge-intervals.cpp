df6e9436d7733363df289d202a24008b6a3c6c54
shangrex <hshangrex@gmail.com>
2026-06-11 09:58:35 -0700

    Time: 3 ms (89.54%), Space: 23.8 MB (81.77%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        sort(intervals.begin(), intervals.end(), [](const vector<int> &a, const vector<int> &b){
-            return a[0] < b[0];
-        });
+        // sort(intervals.begin(), intervals.end(), 
+        // [](const vector<int> &a, const vector<int> &b){
+        //     return a[0] < b[0];
+        // });
+        sort(intervals.begin(), intervals.end());
         int left = intervals[0][0], right = intervals[0][1];
-        
         
         // for(auto i : intervals)cout << i[0] << endl;
         vector<vector<int>> rst;
