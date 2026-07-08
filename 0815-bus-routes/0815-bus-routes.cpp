c061e6672282cedd7f5f7e2d94ceae3a3ecfdbfb
shangrex <hshangrex@gmail.com>
2026-07-08 15:39:04 -0700

    Time: 123 ms (81.2%), Space: 73.1 MB (42.4%) - LeetHub

---
+++ 0815-bus-routes/0815-bus-routes.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    int numBusesToDestination(vector<vector<int>>& routes, int source, int target) {
+        unordered_map<int, vector<int>> graph;
+        unordered_map<int, int> visited;
+
+        queue<int>q;
+
+        for(int i = 0; i < routes.size(); i++){
+            for(int j = 0; j < routes[i].size(); j++){
+                graph[routes[i][j]].push_back(i);
+            }
+        }
+        // for(auto &i : graph[source]){
+        //     q.push(i);
+        //     visited[i] = 1;
+        // }
+        q.push(source);
+        visited[source] = 1;
+  
+        int level = 0;
+        while(!q.empty()){
+            int n = q.size();
+            level ++;
+            for(int i = 0; i < n; i++){
+                int cur = q.front();
+                q.pop();
+                if(cur == target){
+                    return level-1;
+                }
+                for(auto &j : graph[cur]){
+                    for(auto &k : routes[j]){
+                        if(visited.find(k) == visited.end()){
+                            visited[k] = 1;
+                            q.push(k);
+                        }
+                    }
+                    routes[j].clear();
+                }
+
+            }
+        }
+
+        return -1;
+    }
+};
