f4c23ba6b2c30f5e80a4c17e04310ea90fdc2c29
shangrex <hshangrex@gmail.com>
2026-07-08 09:57:26 -0700

    Time: 4 ms (87.64%), Space: 22.9 MB (47.71%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,34 +1,29 @@
 class Solution {
 public:
     int minEatingSpeed(vector<int>& piles, int h) {
-        // k
-        int eat_speed = 1;
-        int max_eat_speed = 1;
-        
-        // find max
-        for(auto i : piles)max_eat_speed = max(max_eat_speed, i);
-        
-        while(eat_speed < max_eat_speed){
-            long long int total_hours = 0;
-            int middle = (eat_speed+max_eat_speed)/2;
-            
-            for(int i = 0; i < piles.size(); i++){
-                total_hours += (piles[i] / middle);
-                if(piles[i]%middle)total_hours+=1;
+        int speed = 1;
+        int maxSpeed = 1;
+        for(auto &i : piles){
+            maxSpeed = max(i, maxSpeed);
+        }
+        int midSpeed;
+        while(speed < maxSpeed){
+            midSpeed = (speed+maxSpeed)/2;
+
+            int tmpHour = 0;
+            for(auto &i: piles){
+                tmpHour += (i/midSpeed);
+                if(i%midSpeed) tmpHour += 1;
             }
-            // cout << "eat speed" << eat_speed << " " << total_hours << endl;
-            
-            if(total_hours <= h){
-                // success
-                max_eat_speed = middle;
+
+            if(tmpHour > h){
+                speed = midSpeed+1;
             }
             else {
-                eat_speed = middle+1;
+                maxSpeed = midSpeed;
             }
-            
         }
-        
-        return max_eat_speed;
-        
+
+        return speed;
     }
 };
