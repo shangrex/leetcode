b9768c8b7a529098e125b0a2260c8a26ccbbb1b5
shangrex <hshangrex@gmail.com>
2026-07-29 22:26:11 -0700

    Time: 5 ms (12.55%), Space: 14.5 MB (7.59%) - LeetHub

---
+++ 0414-third-maximum-number/0414-third-maximum-number.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int thirdMax(vector<int>& nums) {
+        priority_queue<int, vector<int>, greater<int>> pq;
+        set<int> st;
+        for(int i = 0; i < nums.size(); i++){
+            if(st.count(nums[i]) == 0)pq.push(nums[i]);
+            st.insert(nums[i]);
+            if(pq.size() > 3){
+                pq.pop();
+            }
+        }
+        int n = pq.size();
+        for(int i = 0; i < 3-n; i++){
+            pq.pop();
+        }
+        int ret = pq.top();
+        return ret;
+    }
+};
