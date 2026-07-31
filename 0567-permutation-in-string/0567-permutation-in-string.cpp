320e68f3f73936df1b076c6c9241e62241fdeb7f
shangrex <hshangrex@gmail.com>
2026-07-30 23:07:21 -0700

    Time: 218 ms (10.05%), Space: 10.2 MB (30.69%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,40 +1,29 @@
 class Solution {
 public:
-    
-    bool match(vector<int>&s1, vector<int>&s2){
+    bool match(vector<int>&s1c, vector<int>&s2c){
         for(int i = 0; i < 26; i++){
-            if(s1[i] != s2[i])return false;
+            if(s1c[i] > s2c[i]) return false;
         }
         return true;
     }
-    
     bool checkInclusion(string s1, string s2) {
-        if(s1.size() > s2.size())return false;
-        int left = 0, right = 0;
-        
-        vector<int>count_s1(26, 0);
-        vector<int>count_s2(26, 0);
-        for(int i = 0; i < s1.length(); i++){
-            count_s1[s1[i]-'a']++;
-        }
-        int n1 = s1.length();
-        
-        int n2 = s2.length();   
-        
-        while(right < n2){
-            
-            while(right-left+1 < n1){
-                count_s2[s2[right]-'a']++;
-                right++;
-            }      
-            count_s2[s2[right]-'a']++;
-            
-            if(match(count_s1, count_s2))return true;
-            
-            count_s2[s2[left]-'a']--;
-            left++;
-            right++;
+        vector<int> s1c(26), s2c(26);
 
+        for(auto &i : s1){
+            s1c[i-'a'] +=1;
+        }
+
+        int n = s1.length();
+        int leftIdx = 0, rightIdx = 0;
+        while(rightIdx < s2.length()){
+            s2c[s2[rightIdx]-'a'] += 1;
+            while(leftIdx <= rightIdx && match(s1c, s2c)){
+                cout << leftIdx << ' ' << rightIdx << endl;
+                if(rightIdx - leftIdx == n-1) return true;
+                s2c[s2[leftIdx]-'a'] --;
+                leftIdx++;
+            }
+            rightIdx++;
         }
         return false;
     }
