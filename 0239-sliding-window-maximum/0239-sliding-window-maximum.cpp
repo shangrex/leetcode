f36a1f3b85e17a9f80b334a50d7a692a12a75423
shangrex <hshangrex@gmail.com>
2026-07-31 00:05:39 -0700

    Time: 19 ms (74.77%), Space: 139.2 MB (70.74%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,24 +1,37 @@
 class Solution {
 public:
+    /*
+    Approach 1. monothonic dequeue + sliding window
+        dequeue store the index of nums
+        dequeu and pop both side
+        ret look after dequeue front
+        add value look after dequee tail
+    */
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        vector<int>rst;
-        deque<int>dq;
+        deque<int> dq;
+        vector<int> ret;
+
         for(int i = 0; i < k; i++){
-            while(!dq.empty()  && nums[dq.back()] < nums[i]){
-                dq.pop_back();
-            }
-            dq.push_back(i);
-        }
-        rst.push_back(nums[dq.front()]);
-        
-        for(int i = k; i < nums.size(); i++){
-            if(i - dq.front() >= k) dq.pop_front();
             while(!dq.empty() && nums[dq.back()] < nums[i]){
                 dq.pop_back();
             }
             dq.push_back(i);
-            rst.push_back(nums[dq.front()]);
         }
-        return rst;
+        ret.push_back(nums[dq.front()]);
+
+        
+        for(int i = k; i < nums.size(); i++){
+            // dq 2 k = 3
+            // 3 4 5 
+            while(!dq.empty() && i - dq.front() > k-1){
+                dq.pop_front();
+            }
+            while(!dq.empty() && nums[dq.back()] < nums[i]){
+                dq.pop_back();
+            }
+            dq.push_back(i);
+            ret.push_back(nums[dq.front()]);
+        }
+        return ret;
     }
 };
