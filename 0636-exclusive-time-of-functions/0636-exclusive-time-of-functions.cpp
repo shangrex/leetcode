967bf0c9a1b235914e0c8a0778fbbbc7eb35a52b
shangrex <hshangrex@gmail.com>
2026-08-01 16:41:17 -0700

    Time: 11 ms (24.22%), Space: 18.2 MB (36.18%) - LeetHub

--- 0636-exclusive-time-of-functions/0636-exclusive-time-of-functions.cpp
+++ 0636-exclusive-time-of-functions/0636-exclusive-time-of-functions.cpp
@@ -7,6 +7,42 @@ struct event{
 class Solution {
 public:
     vector<int> exclusiveTime(int n, vector<string>& logs) {
+        vector<int> ret(n, 0);
+        stack<event> st;
+        for(int i = 0; i < logs.size(); i++){
+            stringstream ss(logs[i]);
+            int id, timestamp;
+            string status;
+            string line;
+            getline(ss, line, ':');
+            id = stoi(line);
+            getline(ss, line, ':');
+            status = line;
+            getline(ss, line, ':');
+            timestamp = stoi(line);
+
+            event e = {id, status, timestamp};
+
+            if(status == "start"){
+                st.push(e);
+            }
+            else {
+                auto tope = st.top();
+                st.pop();
+                int diff = timestamp - tope.timestamp+1;
+                ret[id] += diff;
+
+                if(!st.empty()){
+                    ret[st.top().id] -= diff;
+                }
+            }
+        }
+        return ret;
+    }
+};
+
+/*
+    vector<int> exclusiveTime(int n, vector<string>& logs) {
         stringstream ss;        
         int pos = 0;
         vector<event> e;
@@ -52,4 +88,4 @@ public:
 
         return ret;
     }
-};
+*/
