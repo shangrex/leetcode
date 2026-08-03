2b3b8d74401a9a9b84c08321a5a7bc09ad3dde2e
shangrex <hshangrex@gmail.com>
2026-08-03 10:06:36 -0700

    Time: 27 ms (10.96%), Space: 38.6 MB (26.46%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
@@ -1,7 +1,29 @@
 class Solution {
 public:
     int numSubarraysWithSum(vector<int>& nums, int goal) {
-        return atMost(nums, goal) - atMost(nums, goal-1);
+        /*
+        Approach 1. sliding window
+        atMost - atMost
+
+        Approach 2. prefix sum
+
+        */
+
+        unordered_map<int, int> mp;
+        
+        long long prefix = 0;
+        int ret = 0;
+        for(int i = 1; i <= nums.size(); i++){
+            prefix += nums[i-1];
+            if(prefix == goal) ret++;
+
+            if(mp.find(prefix - goal) != mp.end()){
+                ret += mp[prefix - goal];
+            }
+            mp[prefix]++;
+        }
+        return ret;
+        // return atMost(nums, goal) - atMost(nums, goal-1);
     }
 
     int atMost(vector<int> &nums, int goal){
